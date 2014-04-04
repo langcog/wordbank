e68f2907fc726361dbbe316c06b234061d98d8f4
Ranjay Krishna <rak248@cornell.edu>
2014-04-03 19:31:18 -0700

    started creatind model classes

--- common/models.py
+++ common/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Child(models.Model):
+  study_id = models.IntegerField()
+  dob = models.DateField()
+  birth_weight = models.FloatField()
+  state = models.CharField(max_length=2)
+  gestational_age = models.IntegerField()
+  #mom_ed
+  #dad_ed
+  #birth_order
+
+class InstrumentsMap(models.Model):
+  name = models.CharField(max_length=20)
+  language = models.CharField(max_length=20)
+
+class Source(models.Model):
+  name = models.CharField(max_length=20)
+  citation = models.CharField(max_length=20)
+  year = models.IntegerField()
+
