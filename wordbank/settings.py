a7767bda1bcb51ec8b823daff9977123e2b5a026
Ranjay Krishna <rak248@cornell.edu>
2014-04-10 13:11:07 -0700

    adding app to handle instruments

--- wordbank/settings.py
+++ wordbank/settings.py
@@ -125,6 +125,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'common',
+    'instruments',
 )
 
 # A sample logging configuration. The only tangible logging
