840116fc77b058d88368ed8b2d30f7e5227a525c
Ubuntu <ubuntu@ip-172-31-0-195.us-west-2.compute.internal>
2014-04-11 22:11:38 +0000

    adding more depencies to the setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,6 +8,11 @@ sudo apt-get install libapache2-mod-wsgi
 sudo apt-get install git-core
 sudo apt-get install gcc
 sudo apt-get install make
+sudo apt-get install subversion
+sudo apt-get update
+sudo apt-get install libpcre3 libpcre3-dev
+sudo apt-get install apache2-dev
+sudo apt-get install python-dev
 
 wget http://www.djangoproject.com/download/1.4.2/tarball/ --max-redirect=2 --trust-server-names
 tar -xvzf Django-1.4.2.tar.gz
@@ -34,9 +39,17 @@ cd ..
 #Install Apache
 wget http://www.gaidso.com/apache//httpd/httpd-2.4.9.tar.bz2
 tar -xvjpf httpd-2.4.9.tar.bz2
-./httpd-2.4.9/configure
+cd httpd-2.4.9
+./configure
+make
+sudo make install
+cd ..
 
 #Install mod-wsgi
-wget https://code.google.com/p/modwsgi/downloads/list/mod_wsgi-3.4.tar.gz
+wget modwsgi.googlecode.com/files/mod_wsgi-3.4.tar.gz
 tar -xvzf mod_wsgi-3.4.tar.gz
-./mod_wsgi-3.4/configure
+cd mod_wsgi-3.4
+./configure
+make
+sudo make install
+cd ..
