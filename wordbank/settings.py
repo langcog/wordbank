8dfd9402a67ebf5d41c7b136d633fad4e0770884
Ubuntu <ubuntu@ip-172-31-0-195.us-west-2.compute.internal>
2014-04-18 08:19:56 +0000

    production server settings

--- wordbank/settings.py
+++ wordbank/settings.py
@@ -23,7 +23,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['54.186.212.168']
+ALLOWED_HOSTS = ['54.186.212.168', '172.31.0.195']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
