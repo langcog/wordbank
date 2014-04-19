5fdc493be25234d51cc77d0df71e6391330883d8
Ubuntu <ubuntu@ip-172-31-0-195.us-west-2.compute.internal>
2014-04-19 01:44:41 +0000

    enabled static files

--- wordbank/settings.py
+++ wordbank/settings.py
@@ -67,7 +67,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'sitestatic'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -79,7 +79,7 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     'static/',
-    os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static'),
+    #os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static'),
     os.path.join(os.path.dirname(__file__), '..', 'static').replace('\\','/'),
 )
 
