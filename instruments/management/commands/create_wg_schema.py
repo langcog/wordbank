a06f54991047f167c7979dfec9aa5e754ab70043
Ranjay Krishna <rak248@cornell.edu>
2014-04-23 11:59:17 -0700

    create wg schema script complete

--- instruments/management/commands/create_wg_schema.py
+++ instruments/management/commands/create_wg_schema.py
@@ -3,21 +3,45 @@ import xlrd
 
 class Command(NoArgsCommand):
 
+  def extract_base(self, index, col_names):
+    if index == len(col_names)-1:
+      return (col_names[index], 1)
+    col1 = col_names[index]
+    col2 = col_names[index+1]
+    if col1[0:len(col1)-1] == col2[0:len(col2)-1] and \
+      (col1[len(col1)-1] == 'p' and col2[len(col2)-1] == 'u' or \
+      col2[len(col2)-1] == 'u' and col2[len(col2)-1] == 'p'):
+      base = col1[0:len(col1)-1]
+      index = 2
+    elif col1[1:] == col2[1:] and \
+      (col1[0] == 'p' and col2[0] == 'u' or \
+      col2[0] == 'u' and col2[0] == 'p'):
+      base = col1[1:]
+      index = 2
+    else:
+      base = col1
+      index = 1
+    return (base, index)
+
   def handle(self, *args, **options):
     book = xlrd.open_workbook('raw_data/CDI-WG.xlsx')
 
     sh = book.sheet_by_index(0)
     nrows = sh.nrows
     ncols = sh.ncols
+    col_names = list(sh.row_values(0))
     
     f = open('instruments/wg.py', 'w')
     f.write('from django.db import models\n')
-    f.write('\nclass WS(models.Model):\n')
+    f.write('\nclass WG(models.Model):\n')
     start = False
-    base = ''
-    for value in sh.row_values(0):
-      if value == 'baabaap':
+    index = 0
+    while index < ncols:
+      if col_names[index] == 'baabaap':
         start = True
-      if 
+      (name, offset) = self.extract_base(index, col_names)
       if start:
-        f.write('  col_' + value + ' = models.IntegerField(null=True, blank=True)\n')
+        f.write('  col_' + name + ' = models.IntegerField(null=True, blank=True)\n')
+        index = index + offset
+      else:
+        index = index + 1
