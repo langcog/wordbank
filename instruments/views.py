2f33b358d3229059d6bc744fcd7dbed62b7f7e31
Ranjay Krishna <rak248@cornell.edu>
2014-05-04 15:25:43 -0700

    adding code to handle bankend of the stats page

--- instruments/views.py
+++ instruments/views.py
@@ -8,12 +8,32 @@ from instruments.models import *
 class Stats(View):
 
   def get(self, request):
-    #for field in WS._meta.fields:
-    
-    return render(request, 'stats.html', {})
+    data = []
+    administrations = Administration.objects.all()
+    for administration in administrations: 
+      obj = {'date_of_test': administration.date_of_test,
+             'age': administration.age,
+             'gender': administration.child.gender,
+             'date_of_birth': administration.child.date_of_birth,
+             'mom_ed': administration.child.mom_ed}  
+      instrument = administration.instrument.name
+      for subclass in BaseTable.__subclasses__():
+        if instrument == subclass.__name__:
+          instrument_class = subclass
+          break
+      instrument_obj = instrument_class.objects.get(pk=administration.data_id).__dict__
+      total = 0
+      for field in instrument_class._meta.fields:
+        field_name = field.get_attname_column()[0]
+        if field_name.startswith('col_'):
+          total = total + instrument_obj[field_name]
+      obj['total'] = total
+      data.append(obj)
+    return render(request, 'stats.html', {'data': data})
 
 
 class Search(View):
-
+  
   def get(self, request):
     return render(request, 'search.html', {})
+
