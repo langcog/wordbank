f15595762df9c6960e63c9857287e4c277da35e1
Ranjay Krishna <rak248@cornell.edu>
2014-05-03 19:46:35 -0700

    starting work on stats:

--- instruments/views.py
+++ instruments/views.py
@@ -2,6 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render, render_to_response
 from django.views.generic import View
 from common.models import *
+from instruments.models import *
+
+
+class Stats(View):
+
+  def get(self, request):
+    #for field in WS._meta.fields:
+    
+    return render(request, 'stats.html', {})
+
 
 class Search(View):
 
