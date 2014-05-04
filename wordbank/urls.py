808ded9637d48859e14c8fb462cbf7e84720b43c
Ranjay Krishna <rak248@cornell.edu>
2014-05-03 19:46:35 -0700

    starting work on stats:

--- wordbank/urls.py
+++ wordbank/urls.py
@@ -9,7 +9,8 @@ from instruments.views import *
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', Home.as_view(), name='home'),
-    url(r'search', views.search, name='search'),
+    url(r'search', Search.as_view(), name='search'),
+    url(r'stats', Stats.as_view(), name='stats'),
     # url(r'^wordbank/', include('wordbank.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
