fcffb6208091236e250e9a3d7443d88d7cd1fbdf
Ranjay Krishna <rak248@cornell.edu>
2014-05-12 20:52:43 -0700

    adding source and ethnicity scripts debugged

--- common/management/commands/populate_ethnicity.py
+++ common/management/commands/populate_ethnicity.py
@@ -1,13 +1,13 @@
 from django.core.management.base import NoArgsCommand
 from common.models import *
 
-class Command(NoArgsComman):
+class Command(NoArgsCommand):
 
   def handle(self, *args, **options):
     ethnicities = {1: "Asian",
                    2: "Black/African American",
                    3: "Hispanic",
                    4: "Other/Mixed"}
-    for k in ethinicities:
+    for k in ethnicities:
       Ethnicity.objects.create(id=k, ethnicity=ethnicities[k])
 
