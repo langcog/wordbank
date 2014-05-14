ae5dcd2ff93d5673680b2a99c08cf4638d743888
Ranjay Krishna <rak248@cornell.edu>
2014-05-13 23:03:39 -0700

    added sources and ethnicities

--- scripts/reset_tables.sh
+++ scripts/reset_tables.sh
@@ -1,7 +1,7 @@
 ./manage.py delete_all_tables
 ./manage.py initialize_all_tables
-./manage.py populate_ethnicity.py
-./manage.py populate_source.py
+./manage.py populate_ethnicity
+./manage.py populate_source
 ./manage.py import_ws
 ./manage.py import_wg
 ./manage.py aggregate_stats
