77bb65d1e38a8821285f60f71b02a2c07159d0a6
Benji Nguyen <bhhnguyen@gmail.com>
2014-05-23 10:38:11 -0700

    updated search function, still need to implement html

--- common/views.py
+++ common/views.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.shortcuts import render, render_to_response
 from django.views.generic import View
 from common.models import *
+from instruments.helper import *
 import csv
 
 class Home(View):
@@ -28,27 +29,28 @@ class Search(View):
   def search(request):
     if request.GET:
       all_admin = Administrator.objects.all()
-    if 'source_name' in request.GET:
-      all_admin = all_admin.filter(Source__name=request.GET['source_name'])
-    if 'source_year' in request.GET:
-      all_admin = all_admin.filter(Source__year=int(request.GET['source_year']))
-    #if 'gender' in request.GET:
-      #all_admin.filter(gender = request.GET['gender'])
-    if 'gestational_age' in request.GET:
-      all_admin = all_admin.filter(Child__gestational_age=int(request.GET['gestational_age']))
-    if 'mom_ed' in request.GET:
-      all_admin = all_admin.filter(Child__mom_ed=int(request.GET['mom_ed']))
-    if 'birth_order' in request.GET:
-      all_admin = all_admin.filter(Child__birth_order=int(request.GET['birth_order']))
-    if 'instrument' in request.GET:
-      all_admin = all_admin.filter(InstrumentsMap__name=request.GET['instrument'])
-    if 'language' in request.GET:
-      all_admin = all_admin.filter(InstrumentsMap__language=request.GET['language'])
-    dump(all_admin.iterator(),'./raw_data/test.csv')
-    #cursor = connection.cursor()
-    #for instrument in all_admin.values_list('InstrumentsMap__name').distinct:
-    #CODE WHERE WE GET ROWS FROM SPECIFIC INSTRUMENT TABLES
-    #dump(all_admin.iterator(), './raw_data/test.csv')
+      if 'dob1' in request.GET:
+        all_admin = all_admin.filter(Child__date_of_birth__gte=request.GET['dob1'])
+        all_admin = all_admin.filter(Child__date_of_birth__lte=request.GET['dob2'])
+      if 'source_name' in request.GET:
+        all_admin = all_admin.filter(Source__name=request.GET['source_name'])
+      if 'source_year' in request.GET:
+        all_admin = all_admin.filter(Child__date_of_birth__gte=request.GET['source_year1'])
+        all_admin = all_admin.filter(Source__year__lte=request.GET['source_year2'])
+      #if 'gender' in request.GET:
+        #all_admin.filter(gender = request.GET['gender'])
+      if 'gestational_age' in request.GET:
+        all_admin = all_admin.filter(Child__gestational_age__gte=int(request.GET['gest_age'1]))
+        all_admin = all_admin.filter(Child__gestational_age__lte=int(request.GET['gest_age'2]))
+      if 'mom_ed' in request.GET:
+        all_admin = all_admin.filter(Child__mom_ed__gte=int(request.GET['mom_ed1']))
+        all_admin = all_admin.filter(Child__mom_ed__lte=int(request.GET['mom_ed2']))
+      if 'birth_order' in request.GET:
+        all_admin = all_admin.filter(Child__birth_order=int(request.GET['birth_order']))
+      if 'instrument' in request.GET:
+        all_admin = all_admin.filter(InstrumentsMap__name=request.GET['instrument'])
+      data = aggregate1(all_admin)
+      return render(request, 'search.html', {})
     return render(request, 'search.html', {})
 
   def dump(qs, path):
