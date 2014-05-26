2361def69f07b075187909ba4d092ae3e067ea7f
ranjaykrishna <rak248@cornell.edu>
2014-05-26 14:50:21 -0700

    added imports to reset script

--- scripts/reset_tables.sh
+++ scripts/reset_tables.sh
@@ -3,5 +3,7 @@
 ./manage.py populate_ethnicity
 ./manage.py populate_source
 ./manage.py import_ws "raw_data/CDI-WS-2.xlsx"
+./manage.py import_ws "raw_data/MarchmanDallas.xlsx"
+./manage.py import_ws "raw_data/MarchmanWisconsin.xlsx"
 ./manage.py import_wg "raw_data/CDI-WG-2.xlsx"
 ./manage.py aggregate_stats
