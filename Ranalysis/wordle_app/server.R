5e5a02a40b37ac95769a581cb1a2ab81ef4a7e0c
Michael C. Frank <mcfrank@stanford.edu>
2014-06-02 15:44:58 -0700

    wordle changes

--- Ranalysis/wordle_app/server.R
+++ Ranalysis/wordle_app/server.R
@@ -5,9 +5,11 @@ shinyServer(function(input, output) {
   
   wordcloud_rep <-  repeatable(wordcloud)
   output$plot <- renderPlot({
-    with(filter(wordle.data,age==input$age),
+    with(subset(wordle.data, wordle.data$age==input$age),
          wordcloud_rep(word,floor(vocab*100),
-                       scale=c(2,.02),min.freq=10,
+                       scale=c(2,.02),
+                       min.freq = input$freq, 
+                       max.words=input$max,
                        colors=brewer.pal(8, "Dark2")))
   })
   
--- Ranalysis/wordle_app/ui.R
+++ Ranalysis/wordle_app/ui.R
@@ -1,8 +1,8 @@
 library(shiny)
 #library(shinyIncubator)
- 
+
 shinyUI(fluidPage(
- 
+  
   # Application title
   titlePanel("Word Cloud"),
   
@@ -10,12 +10,17 @@ shinyUI(fluidPage(
     sidebarPanel(
       sliderInput("age", 
                   "Age (months):", 
-                  min = 16,  max = 30, value = 16)
-    ),
+                  min = 16,  max = 30, value = 16),
+      sliderInput("freq", 
+                  "Minimum Frequency:", 
+                  min = 1,  max = 100, value = 50),
+      sliderInput("max", 
+                  "Maximum Number of Words:", 
+                  min = 1,  max = 100,  value = 20)),
     
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("plot")
+      # Show a plot of the generated distribution
+      mainPanel(
+        plotOutput("plot")
+      )
     )
-  )
-))
+  ))
