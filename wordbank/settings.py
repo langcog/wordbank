75b11773ebaa71b56f601a2a600ea9eaa9071885
Benji Nguyen <bhhnguyen@gmail.com>
2014-06-30 17:12:44 -0700

    search testing 6

--- wordbank/settings.py
+++ wordbank/settings.py
@@ -56,7 +56,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media').replace('\\','/')
+MEDIA_ROOT = ''
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
