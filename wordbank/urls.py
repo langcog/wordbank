06e94837946a8ff84ffc28d62e3cb1ad1893c295
Ranjay Krishna <rak248@cornell.edu>
2014-10-31 14:11:51 -0700

    making UI changes and adding ability to download data

--- wordbank/urls.py
+++ wordbank/urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
     url(r'reports', Reports.as_view(), name='reports'),
     url(r'contribute', Contribute.as_view(), name='contribute'),
     url(r'survey', Survey.as_view(), name='survey'),
-    # url(r'^wordbank/', include('wordbank.foo.urls')),
+    url(r'download', Download.as_view(), name='download'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
