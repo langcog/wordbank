f67dacd2534b7427ec618524189049fb93a667ce
Mika Braginsky <mikabr@stanford.edu>
2014-11-12 19:23:22 -0800

    redid migrations because they broke horribly and were slow/redundant anyway

--- common/migrations/0001_initial.py
+++ common/migrations/0001_initial.py
@@ -1,46 +1,76 @@
 # encoding: utf8
+from __future__ import unicode_literals
+
 from django.db import models, migrations
 
 
 class Migration(migrations.Migration):
-    
-    dependencies = []
+
+    dependencies = [
+    ]
 
     operations = [
         migrations.CreateModel(
-            fields = [(u'id', models.AutoField(verbose_name=u'ID', serialize=False, auto_created=True, primary_key=True),), ('study_id', models.CharField(max_length=20),), ('gender', models.CharField(max_length=1),), ('date_of_birth', models.DateField(null=True, blank=True),), ('birth_weight', models.FloatField(null=True, blank=True),), ('state', models.CharField(max_length=2, null=True, blank=True),), ('gestational_age', models.IntegerField(null=True, blank=True),), ('mom_ed', models.IntegerField(null=True, blank=True),), ('dad_ed', models.IntegerField(null=True, blank=True),), ('birth_order', models.IntegerField(null=True, blank=True),)],
-            bases = (models.Model,),
-            options = {},
-            name = 'Child',
+            name='InstrumentsMap',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=20)),
+                ('language', models.CharField(max_length=20)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
         ),
         migrations.CreateModel(
-            fields = [(u'id', models.AutoField(verbose_name=u'ID', serialize=False, auto_created=True, primary_key=True),), ('name', models.CharField(max_length=20),), ('language', models.CharField(max_length=20),)],
-            bases = (models.Model,),
-            options = {},
-            name = 'InstrumentsMap',
+            name='Ethnicity',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('ethnicity', models.CharField(max_length=30)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
         ),
         migrations.CreateModel(
-            fields = [(u'id', models.AutoField(verbose_name=u'ID', serialize=False, auto_created=True, primary_key=True),), ('lemma', models.CharField(max_length=20),), ('instrument', models.IntegerField(),), ('short_column', models.CharField(max_length=8),), ('long_column', models.CharField(max_length=20),)],
-            bases = (models.Model,),
-            options = {},
-            name = 'WordMapping',
+            name='Source',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=20)),
+                ('dataset', models.CharField(max_length=20, null=True, blank=True)),
+                ('instrument', models.CharField(max_length=20, null=True, blank=True)),
+                ('citation', models.CharField(max_length=50)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
         ),
         migrations.CreateModel(
-            fields = [(u'id', models.AutoField(verbose_name=u'ID', serialize=False, auto_created=True, primary_key=True),), ('name', models.CharField(max_length=20),), ('citation', models.CharField(max_length=20),), ('year', models.IntegerField(),)],
-            bases = (models.Model,),
-            options = {},
-            name = 'Source',
+            name='WordInfo',
+            fields=[
+                ('uni_lemma', models.CharField(max_length=20, serialize=False, primary_key=True)),
+                ('lang_lemma', models.CharField(max_length=20)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
         ),
         migrations.CreateModel(
-            fields = [(u'id', models.AutoField(verbose_name=u'ID', serialize=False, auto_created=True, primary_key=True),), ('lemma', models.CharField(max_length=20),), ('CDI_cat', models.CharField(max_length=20),)],
-            bases = (models.Model,),
-            options = {},
-            name = 'WordInfo',
-        ),
-        migrations.CreateModel(
-            fields = [(u'id', models.AutoField(verbose_name=u'ID', serialize=False, auto_created=True, primary_key=True),), ('child', models.ForeignKey(to='common.Child', to_field=u'id'),), ('instrument', models.ForeignKey(to='common.InstrumentsMap', to_field=u'id'),), ('source', models.ForeignKey(to_field=u'id', blank=True, to='common.Source', null=True),), ('date_of_test', models.DateField(null=True, blank=True),), ('data_id', models.IntegerField(),), ('age', models.IntegerField(),)],
-            bases = (models.Model,),
-            options = {},
-            name = 'Administration',
+            name='Child',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('study_id', models.CharField(max_length=20)),
+                ('gender', models.CharField(max_length=1, null=True, blank=True)),
+                ('date_of_birth', models.DateField(null=True, blank=True)),
+                ('birth_weight', models.FloatField(null=True, blank=True)),
+                ('state', models.CharField(max_length=2, null=True, blank=True)),
+                ('gestational_age', models.IntegerField(null=True, blank=True)),
+                ('mom_ed', models.IntegerField(null=True, blank=True)),
+                ('dad_ed', models.IntegerField(null=True, blank=True)),
+                ('birth_order', models.IntegerField(null=True, blank=True)),
+                ('ethnicity', models.ForeignKey(to_field='id', blank=True, to='common.Ethnicity', null=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
         ),
     ]
