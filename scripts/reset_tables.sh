f31b3a2d2fabe2c734daad37f1afb46823f50f96
Mika Braginsky <mikabr@stanford.edu>
2014-12-09 21:02:56 -0800

    added support for split column WG format, renamed data files

--- scripts/reset_tables.sh
+++ scripts/reset_tables.sh
@@ -13,17 +13,25 @@ echo "Populating Word Mapping..."
 echo "Populating Source..."
 ./manage.py populate_source
 
-while read instrument; do
-    for data_file in raw_data/${instrument}/*; do
-        s=${data_file}
-        s=${s##*/}
-        s=${s%.*}
-        if [[ $s != '['*']' ]] && [[ $s != '~'* ]]; then
-            echo "Importing $s..."
+while read dataset; do
+    s=${dataset}
+    s=${s##*/}
+    s=${s%.*}
+    echo "Importing $s...";
+    ./manage.py import_data $dataset
+done < raw_data/datasets.txt
+#while read instrument; do
+#    for dataset in datasets; do
+#    for data_file in raw_data/${instrument}/*; do
+#        s=${data_file}
+#        s=${s##*/}
+#        s=${s%.*}
+#        if [[ $s != '['*']' ]] && [[ $s != '~'* ]]; then
+#            echo "Importing $s..."
 #                ./manage.py import_data ${data_file}
-        fi
-    done
-done < raw_data/instruments.txt
+#        fi
+#    done
+#done < raw_data/instruments.txt
 
 echo "Aggregating Stats..."
 ./manage.py aggregate_stats
