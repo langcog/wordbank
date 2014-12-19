510846d0ddc60f9de63ea13e9686fc3652ffc11e
Mika Braginsky <mikabr@stanford.edu>
2014-12-18 17:10:23 -0800

    added lexical categories and included non-word items in word mapping

--- instruments/management/commands/populate_word_mapping.py
+++ instruments/management/commands/populate_word_mapping.py
@@ -14,7 +14,7 @@ class Command(NoArgsCommand):
 
             instrument_language, instrument_form = instrument['language'], instrument['form']
             instruments_map = InstrumentsMap.objects.get(form=instrument_form, language=instrument_language)
-
+            print "    Populating items for", instrument_language, instrument_form
             book = xlrd.open_workbook(instrument['file'])
 
             sheet = book.sheet_by_index(0)
@@ -23,22 +23,35 @@ class Command(NoArgsCommand):
             for row in xrange(1, sheet.nrows):
 
                 row_values = list(sheet.row_values(row))
+                item = row_values[col_names.index('item')]
                 item_type = row_values[col_names.index('type')]
+                item_category = row_values[col_names.index('category')]
 
                 if item_type == 'word':
 
-                    item = row_values[col_names.index('item')]
-                    category = row_values[col_names.index('category')]
                     lang_lemma = row_values[col_names.index('lang_lemma')]
                     uni_lemma = row_values[col_names.index('uni_lemma')]
                     definition = row_values[col_names.index('definition')]
+                    lexical_category = row_values[col_names.index('lexical_category')]
 
                     if not WordInfo.objects.filter(uni_lemma=uni_lemma, lang_lemma=lang_lemma).exists():
                         WordInfo.objects.create(uni_lemma=uni_lemma, lang_lemma=lang_lemma)
                     word_info = WordInfo.objects.get(uni_lemma=uni_lemma, lang_lemma=lang_lemma)
 
                     WordMapping.objects.create(item=item,
-                                               definition=definition,
-                                               category=category,
                                                instrument=instruments_map,
-                                               word_info=word_info)
+                                               type=item_type,
+                                               category=item_category,
+                                               word_info=word_info,
+                                               definition=definition,
+                                               lexical_category=lexical_category)
+
+                else:
+
+                    WordMapping.objects.create(item=item,
+                                               instrument=instruments_map,
+                                               type=item_type,
+                                               category=item_category,
+                                               word_info=None,
+                                               definition=None,
+                                               lexical_category=None)
