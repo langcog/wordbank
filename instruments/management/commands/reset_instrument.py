d5ddec79d82f7e050dd7e2d25606f8d54227c6eb
Mika Braginsky <mikabr@stanford.edu>
2015-01-26 12:47:49 -0800

    item definitions for grammar items

--- instruments/management/commands/reset_instrument.py
+++ instruments/management/commands/reset_instrument.py
@@ -1,1 +1,24 @@
-__author__ = 'mikabr'
+from django.core.management.base import NoArgsCommand
+from common.models import *
+import instruments.models
+
+
+class Command(NoArgsCommand):
+
+    def handle(self, *args, **options):
+
+        instrument_language, instrument_form = args[0], args[1]
+
+        instrument_model = getattr(instruments.models, '_'.join([instrument_language, instrument_form]))
+        instrument_obj = InstrumentsMap.objects.get(language=instrument_language, form=instrument_form)
+
+        # broken for now, do not use
+        # for administration in Administration.objects.filter(instrument=instrument_obj.pk):
+        #     Child.objects.get(pk=administration.child.pk).delete()
+        #     administration.delete()
+        #
+        # for word_map in WordMapping.objects.filter(instrument=instrument_obj.pk):
+        #     WordInfo.objects.get(pk=word_map.word_info.pk).delete()
+        #     word_map.delete()
+        #
+        # instrument_obj.delete()
