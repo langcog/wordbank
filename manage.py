4f59eef27dfbbaabe41b34a2fc607a7e82edd52f
Mika Braginsky <mikabr@stanford.edu>
2015-01-26 12:48:18 -0800

    faulthander in manage.py to see traceback

--- manage.py
+++ manage.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
+import faulthandler
 
 if __name__ == "__main__":
+
+    faulthandler.enable()
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wordbank.settings")
 
     from django.core.management import execute_from_command_line
