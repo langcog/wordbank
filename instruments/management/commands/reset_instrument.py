5b35a0c3a034b7f4ef0c0d6d690a0e3ae8eec3d1
Mika Braginsky <mikabr@stanford.edu>
2015-02-13 10:18:53 -0800

    fixed reset instrument command

--- instruments/management/commands/reset_instrument.py
+++ instruments/management/commands/reset_instrument.py
@@ -3,22 +3,19 @@ from common.models import *
 import instruments.models
 
 
+# deletes all administration and child objects for a given instrument (language and form)
 class Command(NoArgsCommand):
 
     def handle(self, *args, **options):
 
         instrument_language, instrument_form = args[0], args[1]
 
-        instrument_model = getattr(instruments.models, '_'.join([instrument_language, instrument_form]))
-        instrument_obj = InstrumentsMap.objects.get(language=instrument_language, form=instrument_form)
-
-        # broken for now, do not use
-        # for administration in Administration.objects.filter(instrument=instrument_obj.pk):
-        #     Child.objects.get(pk=administration.child.pk).delete()
-        #     administration.delete()
-        #
-        # for word_map in WordMapping.objects.filter(instrument=instrument_obj.pk):
-        #     WordInfo.objects.get(pk=word_map.word_info.pk).delete()
-        #     word_map.delete()
-        #
-        # instrument_obj.delete()
+        if InstrumentsMap.objects.filter(language=instrument_language, form=instrument_form).exists():
+            instrument_obj = InstrumentsMap.objects.get(language=instrument_language, form=instrument_form)
+            for administration in Administration.objects.filter(instrument=instrument_obj.pk):
+                if Child.objects.filter(pk=administration.child.pk).exists():
+                    Child.objects.filter(pk=administration.child.pk).delete()
+                else:
+                    administration.delete()
+        else:
+            raise IOError("Invalid instrument.")
