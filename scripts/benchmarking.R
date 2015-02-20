f2719421ecf60a676a96c09df2c8400717adb281
Mika Braginsky <mikabr@stanford.edu>
2015-02-20 15:26:20 -0800

    local/remote benchmarking

--- scripts/benchmarking.R
+++ scripts/benchmarking.R
@@ -1,32 +1,42 @@
-rm(list=ls())
-
 library(dplyr)
 library(tidyr)
 library(RMySQL)
-library(magrittr)
+library(ggplot2)
+library(microbenchmark)
+setwd("~/Documents/projects/wordbank/scripts/benchmarking")
 
-wordbank <- src_mysql(dbname='wordbank')
-#wordbank <- src_mysql(dbname="wordbank", host="54.149.39.46",
-#                      user="wordbank", password="wordbank")
+wordbank.local <- src_mysql(dbname='wordbank')
+#wordbank.remote <- src_mysql(dbname="wordbank", host="54.149.39.46",
+#                             user="wordbank", password="wordbank")
 
 # load tables
 #source.table <- tbl(wordbank, "common_source")
-admin.table <- tbl(wordbank, "common_administration")
-child.table <- tbl(wordbank, "common_child")
-wordmapping.table <- tbl(wordbank, "common_wordmapping")
-instruments.table <- tbl(wordbank, "common_instrumentsmap")
-english.ws.table <- tbl(wordbank, "instruments_english_ws")
-english.wg.table <- tbl(wordbank, "instruments_english_wg")
-spanish.ws.table <- tbl(wordbank, "instruments_spanish_ws")
-spanish.wg.table <- tbl(wordbank, "instruments_spanish_wg")
-norwegian.ws.table <- tbl(wordbank, "instruments_norwegian_ws")
-norwegian.wg.table <- tbl(wordbank, "instruments_norwegian_wg")
-danish.ws.table <- tbl(wordbank, "instruments_danish_ws")
+admin.table <- tbl(wordbank.local, "common_administration")
+child.table <- tbl(wordbank.local, "common_child")
+wordmapping.table <- tbl(wordbank.local, "common_wordmapping")
+instruments.table <- tbl(wordbank.local, "common_instrumentsmap")
+
+english.ws.table.local <- tbl(wordbank.local, "instruments_english_ws")
+english.wg.table.local <- tbl(wordbank.local, "instruments_english_wg")
+spanish.ws.table.local <- tbl(wordbank.local, "instruments_spanish_ws")
+spanish.wg.table.local <- tbl(wordbank.local, "instruments_spanish_wg")
+norwegian.ws.table.local <- tbl(wordbank.local, "instruments_norwegian_ws")
+norwegian.wg.table.local <- tbl(wordbank.local, "instruments_norwegian_wg")
+danish.ws.table.local <- tbl(wordbank.local, "instruments_danish_ws")
+
+#english.ws.table.remote <- tbl(wordbank.remote, "instruments_english_ws")
+#english.wg.table.remote <- tbl(wordbank.remote, "instruments_english_wg")
+#spanish.ws.table.remote <- tbl(wordbank.remote, "instruments_spanish_ws")
+#spanish.wg.table.remote <- tbl(wordbank.remote, "instruments_spanish_wg")
+#norwegian.ws.table.remote <- tbl(wordbank.remote, "instruments_norwegian_ws")
+#norwegian.wg.table.remote <- tbl(wordbank.remote, "instruments_norwegian_wg")
+#danish.ws.table.remote <- tbl(wordbank.remote, "instruments_danish_ws")
 
 # get administration info
 admins <- admin.table %>%
   select(data_id,child_id,age,source_id) %>%
-  rename(id = data_id, child.id = child_id, source.id = source_id) 
+  rename(id = data_id, child.id = child_id, source.id = source_id) %>%
+  transform(id = as.numeric(id))
 child.data <- as.data.frame(admins)
 
 # get word mapping info
@@ -43,24 +53,40 @@ items <- left_join(mapping, instruments) %>%
 
 get.instrument.data <- function(instrument.table, instrument.language, instrument.form) {
   
+#  t <- Sys.time()
+  
   instrument.items <- items %>% 
     filter(language == instrument.language, form == instrument.form) %>%
     select(item_id, item, type, category, lexical_category, definition, complexity_category) %>%
-    mutate(item_id = as.numeric(str_replace(item_id, "item_", "")))
+    mutate(item_id = as.numeric(substr(item_id, 6, nchar(item_id))))
+    
+#  print(paste("instrument.items", Sys.time()-t))
+#  t <- Sys.time()
   
   columns <- colnames(instrument.table)
   instrument.names <- columns[2:length(columns)]
-  
+
+#  print(paste("instrument.names", Sys.time()-t))
+#  t <- Sys.time()
+    
   instrument.data <- as.data.frame(instrument.table) %>%
-    rename(id = basetable_ptr_id) %>%
-  # gather(item, value, -id) %>%
-    gather_("item_id", "value", instrument.names) %>%
-    mutate(item_id = as.numeric(str_replace(item_id, "item_", "")))
-  
+    transform(id = as.numeric(basetable_ptr_id)) %>%
+    gather_("item_id", "value", instrument.names, convert=TRUE) %>%
+    mutate(item_id = as.numeric(substr(item_id, 6, nchar(item_id))))
+
+#  print(paste("instrument.data", Sys.time()-t))
+#  t <- Sys.time()
+
   d <- left_join(instrument.data, instrument.items, by=c("item_id"))
   
-  d <- left_join(d, child.data)
-  return(d)
+#  print(paste("instrument.data + instrument.items", Sys.time()-t))
+#  t <- Sys.time()
+
+  d <- left_join(d, child.data, by=c("id"))
+  
+#  print(paste("instrument.data + instrument.items + child.data", Sys.time()-t))
+
+return(d)
 }
 
 data.loading.time <- function(table, language, form) {
@@ -70,22 +96,43 @@ data.loading.time <- function(table, language, form) {
 return(end-start)
 }
 
+expr <- c("EnglishWS", "EnglishWG", "SpanishWS", "SpanishWG", "NorwegianWS", "NorwegianWG", "DanishWS")
+n <- c(nrow(english.ws.table.local), nrow(english.wg.table.local),
+       nrow(spanish.ws.table.local), nrow(spanish.wg.table.local),
+       nrow(norwegian.ws.table.local), nrow(norwegian.wg.table.local),
+       nrow(danish.ws.table.local))
+ns <- data_frame(expr, n)
 
-language <- c("English", "English", "Spanish", "Spanish", "Norwegian", "Norwegian", "Danish")
-form <- c("WS", "WG", "WS", "WG", "WS", "WG", "WS")
-n <- c(nrow(english.ws.table), nrow(english.wg.table),
-       nrow(spanish.ws.table), nrow(spanish.wg.table),
-       nrow(norwegian.ws.table), nrow(norwegian.wg.table),
-       nrow(danish.ws.table))
-time <- c(data.loading.time(english.ws.table, "English", "WS"),
-          data.loading.time(english.wg.table, "English", "WG"),
-          data.loading.time(spanish.ws.table, "Spanish", "WS"),
-          data.loading.time(spanish.wg.table, "Spanish", "WG"),
-          data.loading.time(norwegian.ws.table, "Norwegian", "WS"),
-          data.loading.time(norwegian.wg.table, "Norwegian", "WG"),
-          data.loading.time(danish.ws.table, "Danish", "WS"))
+local <- microbenchmark(EnglishWS = get.instrument.data(english.ws.table.local, "English", "WS"),
+                        EnglishWG = get.instrument.data(english.wg.table.local, "English", "WG"),
+                        SpanishWS = get.instrument.data(spanish.ws.table.local, "Spanish", "WS"),
+                        SpanishWG = get.instrument.data(spanish.wg.table.local, "Spanish", "WG"),
+                        NorwegianWS = get.instrument.data(norwegian.ws.table.local, "Norwegian", "WS"),
+                        NorwegianWG = get.instrument.data(norwegian.wg.table.local, "Norwegian", "WG"),
+                        DanishWS = get.instrument.data(danish.ws.table.local, "Danish", "WS"),
+                        times=10)
 
-times <- data_frame(language, form, n, time)
+#remote <- microbenchmark(EnglishWS = get.instrument.data(english.ws.table.remote, "English", "WS"),
+#                        EnglishWG = get.instrument.data(english.wg.table.remote, "English", "WG"),
+#                        SpanishWS = get.instrument.data(spanish.ws.table.remote, "Spanish", "WS"),
+#                        SpanishWG = get.instrument.data(spanish.wg.table.remote, "Spanish", "WG"),
+#                        NorwegianWS = get.instrument.data(norwegian.ws.table.remote, "Norwegian", "WS"),
+#                        NorwegianWG = get.instrument.data(norwegian.wg.table.remote, "Norwegian", "WG"),
+#                        DanishWS = get.instrument.data(danish.ws.table.remote, "Danish", "WS"), times=10)
+  
+#times10 <- left_join(local, remote, by=c("expr")) %>%
+#  rename(local = time.x, remote = time.y) %>%
+#  gather(type, time, local, remote) %>%
+#  mutate(time = time*1e-09)
 
-ggplot(times, aes(x=n, y=time)) +
-  geom_point()
+#times10_n <- left_join(times10, ns)
+times_n <- left_join(local, ns) %>%
+  mutate(time = time*1e-09)
+
+write.csv(times_n, "benchmarking.csv")
+
+ggplot(times_n, aes(x=n, y=time), color=type) +
+  geom_point() + 
+  geom_smooth() +
+  theme_bw()
+ggsave("local_benchmark.png")
