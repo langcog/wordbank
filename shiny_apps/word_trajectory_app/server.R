90f22e7f9cb5c0918de0d379c8dd8f649f7f2f34
Mika Braginsky <mikabr@stanford.edu>
2015-02-25 22:21:28 -0800

    cross-report data loading functions and updated word trajectory report

--- shiny_apps/word_trajectory_app/server.R
+++ shiny_apps/word_trajectory_app/server.R
@@ -1,195 +1,148 @@
 ############## STUFF THAT RUNS ONCE WHEN APP LOADS ##############
+rm(list=ls())
 library(shiny)
+library(tidyr)
 library(ggplot2)
+library(directlabels)
 library(dplyr)
 library(RMySQL)
-library(directlabels)
-library(tidyr)
-library(stringr)
-source("app_themes.R")
+source("../app_themes.R")
+source("../data_loading.R")
+#options(shiny.error=NULL)
+Sys.setlocale(locale="C")
 
-# READ IN ENGLISH LABELS FOR WORDS
-wscodes <- read.csv("WS_codes.csv")  %>%
-  arrange(code)
-wgcodes <- read.csv("WG_codes.csv") %>%
-  arrange(code)
+wordbank <- src_mysql(dbname="wordbank")
 
-## OPEN DATABASE CONNECTION ##
-wordbank <- src_mysql(dbname='wordbank',host="54.200.225.86", 
-                     user="wordbank",password="wordbank")
+admin.table <- tbl(wordbank, "common_administration")
+child.table <- tbl(wordbank, "common_child")
+instruments.table <- tbl(wordbank, "common_instrumentsmap")
+momed.table <- tbl(wordbank, "common_momed")
+wordmapping.table <- tbl(wordbank, "common_wordmapping")
 
-## NOW LOAD TABLES ##
-admin.table <- tbl(wordbank,"common_administration")
-child.table <- tbl(wordbank,"common_child")
-ws.table <- tbl(wordbank,"instruments_ws")
-wg.table <- tbl(wordbank,"instruments_wg")
+admins <- get.administration.data(momed.table, child.table, instruments.table, admin.table)
+items <- get.item.data(wordmapping.table, instruments.table)
 
-## MERGE TABLES
-ws.vocab.words <- as.data.frame(select(ws.table,
-                                       basetable_ptr_id,
-                                       col_baabaa:col_connthen)) %>%
-  rename(id = basetable_ptr_id)
-
-wg.vocab.words <- as.data.frame(select(wg.table,
-                                       basetable_ptr_id,
-                                       col_baabaa:col_some)) %>%
-  rename(id = basetable_ptr_id)
-
-### GET DATA FROM INSTRUMENTS AND JOIN UP
-ws.kid.words <- ws.vocab.words %>%
-  gather(word,produces,col_baabaa:col_connthen)
-
-wg.kid.words <- wg.vocab.words %>%
-  gather(word,produces,col_baabaa:col_some) %>%
-  mutate(understands = (produces >= 1),
-    produces = (produces == 2))
-
-## MERGE IN KID DATA
-admins <- as.data.frame(select(admin.table,data_id,child_id,age)) %>%
-  rename(id = data_id, child.id = child_id)
-
-children <- as.data.frame(select(child.table,id,gender,mom_ed,birth_order)) %>%
-  rename(child.id = id)
-
-child.data <- left_join(children,admins)
-
-ws.data <- left_join(ws.kid.words, child.data) %>%
-  mutate(word = str_replace(word, "col_", "")) %>%
-  filter(age >= 16 & age <= 30) %>% 
-  group_by(age,word) %>%
-  summarise(produces = mean(produces))
-
-wg.data <- left_join(wg.kid.words, child.data) %>%
-  mutate(word = str_replace(word, "col_", "")) %>%
-  filter(age >= 8 & age <= 18) %>% 
-  group_by(age,word) %>%
-  summarise(produces = mean(produces),
-            understands = mean(understands))
-
-ws.data$word <- sapply(ws.data$word, 
-                              function(w) wscodes[which(wscodes$code == w),
-                                                  "name"],
-                              USE.NAMES = FALSE)
-
-wg.data$word <- sapply(wg.data$word, 
-                              function(w) wgcodes[which(wgcodes$code == w),
-                                                  "name"],
-                              USE.NAMES = FALSE)
-
-# WORD SELECTORS
-ws.words <- list()
-ws.words$"Select Word" <- ""
-for (i in 1:length(wscodes$name)) {
-  wdname <- paste("\"",wscodes[i,2],"\"",sep="")
-  wdcode <- wscodes[i,2]
-  eval(parse(text=paste("ws.words$",wdname,"=\"",wdcode,"\"",sep="")))
+list.words.by.definition <- function(word.data) {
+  words <- word.data$item.id
+  names(words) <- word.data$definition
+  return(words)
 }
 
-wg.words <- list()
-wg.words$"Select Word" <- ""
-for (i in 1:length(wgcodes$name)) {
-  wdname <- paste("\"",wgcodes[i,2],"\"",sep="")
-  wdcode <- wgcodes[i,2]
-  eval(parse(text=paste("wg.words$",wdname,"=\"",wdcode,"\"",sep="")))
+list.words.by.id <- function(word.data) {
+  words <- word.data$definition
+  names(words) <- word.data$item.id
+  return(words)
 }
 
-############ STUFF THAT RUNS WHEN USER LOADS PAGE ##############
-# to run:
-# runApp("~/Projects/Wordbank/wordbank/Ranalysis/norms_app")
-# input <- list(word1 = "baabaa", word2 = "break", word3 = "cat")
+tables <- get.instrument.tables(instruments.table)
+instrument.tables <- tables %>%
+  group_by(instrument_id) %>%
+  do(words.by.definition = list.words.by.definition(filter(items, instrument_id==.$instrument_id,
+                                                           type=='word')),
+     words.by.id = list.words.by.id(filter(items, instrument_id==.$instrument_id,
+                                           type=='word'))) %>%
+  left_join(tables)
 
-shinyServer(function(input, output) {
+
+## SETUP
+input <- list(language = "Norwegian", form = "WS", measure = "produces",
+              word1 = "item_1", word2 = "item_1", word3 = "item_1")
+
+############## STUFF THAT RUNS WHEN USER CHANGES SOMETHING ##############
+shinyServer(function(input, output) {  
   
-  ############## STUFF THAT RUNS WHEN USER CHANGES SOMETHING ##############
+  instrument <- reactive({filter(instrument.tables,
+                                 language == input$language,
+                                 form == input$form)})
+
+#   instrument <- function() {return(filter(instrument.tables,
+#                                         language == input$language,
+#                                         form == input$form))}
+  
+  ### PLOT RENDERER
   output$plot <- renderPlot({
+
+    instrument.table <- instrument()$table[[1]]
+    instrument.words.by.definition <- instrument()$words.by.definition[[1]]
+    instrument.words.by.id <- instrument()$words.by.id[[1]]
+    words <- c(input$word1, input$word2, input$word3)
+#    columns <- instrument.words.by.definition[c(input$word1, input$word2, input$word3)]
+
+    data <- get.instrument.data(instrument.table, words) %>%
+      filter(measure == input$measure) %>%
+      left_join(admins) %>%
+      group_by(item.id, age) %>%
+      summarise(score = mean(value, na.rm=TRUE)) %>%
+      rowwise %>%
+      mutate(word = instrument.words.by.id[[paste("item_", item.id, sep="")]])
     
-    if(is.null(input$instrument))
-      return()
-    
-    data = ws.data %>%
-      filter(word == input$word1 |
-                     word == input$word2 |
-                     word == input$word3) %>% 
-      group_by(age,word) %>%
-      summarise(p = mean(produces))
-    
-    label = "Proportion of Children Producing"
-    
-    xlims = c(16,32)
-    xbreaks = 16:30
-    
-    if(input$instrument == "WG"){
-      data = wg.data %>%
-        filter(word == input$word1 |
-                 word == input$word2 |
-                 word == input$word3) %>% 
-        group_by(age,word)
-      
-      if(input$measure=="understands"){
-        data <- summarise(data,p=mean(understands))
+    if(input$form == "WG") {
+      if(input$measure == "understands") {
         label <- "Proportion of Children Understanding"
       } else {
-        data <- summarise(data,p=mean(produces))
         label <- "Proportion of Children Producing" 
       }
-      
+      data %<>% filter(age >= 8 & age <= 18)
       xlims = c(8,20)
       xbreaks = 8:18
+    } else {
+      data %<>% filter(age >= 16 & age <= 30)
+      xlims = c(16,32)
+      xbreaks = 16:30
+      label = "Proportion of Children Producing"
     }
-       
-    ggplot(data, aes(x=age, y=p,colour=word,label=word))+
-      geom_smooth(se=FALSE,method="loess") +
-      geom_point()+
-      scale_x_continuous(breaks=xbreaks,
-                         limits=xlims,
-                         name = "Age (months)")+
+    
+    ggplot(data, aes(x=age, y=score, colour=word, label=word)) +
+      geom_smooth(se=FALSE, method="loess") +
+      geom_point() +
+      scale_x_continuous(name = "Age (months)",
+                         breaks = xbreaks,
+                         limits = xlims) +
       scale_y_continuous(name = label,
-                         limits=c(-.01,1),
-                         breaks=seq(0,1,.25)) +
-      theme_bw(base_size=14) + 
+                         limits = c(-.01,1),
+                         breaks = seq(0,1,.25)) +
       theme(legend.position="none") +
-      geom_dl(method = list(dl.trans(x=x +.2),"last.qp",cex=1))+
-      scale_colour_brewer(palette="Set1") 
-  })
-  
-  ### FIELD SELECTOR
-  output$field_selector_control <- renderUI({
-    if (input$instrument == "WG") {
-      fields <- list("Produces" = "produces","Understands" = "understands")
-    } else if (input$instrument == "WS") {
-      fields <- list("Produces" = "produces")
-    }
-    selectInput("measure", h4("Measure"), fields, selectize = TRUE)
+      geom_dl(method = list(dl.trans(x=x +.2),"last.qp",cex=1)) +
+      scale_colour_brewer(palette="Set1")
+    
   })
   
   ### WORD SELECTORS
   output$word1_selector <- renderUI({
-    if (input$instrument == "WG") {
-      words = wg.words
-    } else if (input$instrument == "WS") {
-      words = ws.words
-    }
-    selectInput("word1", h4("Word1"), choices=words, selectize = TRUE,
-                selected = 1)
+    selectizeInput("word1", label = h4("Word1"), 
+                   choices = instrument()$words.by.definition[[1]], selected = 1)
   })
-    
+  
   output$word2_selector <- renderUI({ 
-    if (input$instrument == "WG") {
-      words = wg.words
-    } else if (input$instrument == "WS") {
-      words = ws.words
-    }
-    selectInput("word2", h4("Word2"), choices=words, selectize = TRUE,
-                selected = 1)
+    selectizeInput("word2", label = h4("Word2"), 
+                   choices = instrument()$words.by.definition[[1]], selected = 1)
   })
   
   output$word3_selector <- renderUI({
-    if (input$instrument == "WG") {
-      words = wg.words
-    } else if (input$instrument == "WS") {
-      words = ws.words
-    }
-    selectInput("word3", h4("Word3"), choices=words, selectize = TRUE,
-                selected = 1)
+    selectizeInput("word3", label = h4("Word3"), 
+                   choices = instrument()$words.by.definition[[1]], selected = 1)
   })
+  
+  ### FIELD SELECTORS
+  output$language_selector_control <- renderUI({    
+    selectizeInput("language", label = h4("Language"), 
+                   choices = unique(instrument.tables$language), selected = 1)
+  })
+  
+  output$form_selector_control <- renderUI({    
+    selectizeInput("form", label = h4("Form"), 
+                   choices = unique(filter(instrument.tables,
+                                           language == input$language)$form), selected = 1)
+  })
+    
+  output$measure_selector_control <- renderUI({    
+    if (input$form == "WG") {
+      measures <- list("Produces" = "produces", "Understands" = "understands")
+    } else if (input$form == "WS") {
+      measures <- list("Produces" = "produces")
+    }
+    selectizeInput("measure", label = h4("Measure"), 
+                   choices = measures, selected = 1)
+    })
+  
 })
