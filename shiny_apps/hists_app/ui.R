185e4b9141f9d9edcf8f0a0e02cc01d83917e043
Mika Braginsky <mikabr@stanford.edu>
2015-02-27 00:19:39 -0800

    updated hists app

--- shiny_apps/hists_app/ui.R
+++ shiny_apps/hists_app/ui.R
@@ -1,5 +1,4 @@
 library(shiny)
-#library(shinyIncubator)
 
 shinyUI(fluidPage(
   
@@ -8,24 +7,23 @@ shinyUI(fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      selectInput("instrument", label = h4("Instrument"), 
-                  choices = list("Words and Gestures" = "WG", "Words and Sentences" = "WS"), 
-                  selected = 1),
-      selectInput("measure", label = h4("Measure"), 
-                  choices = list("Understands" = "understands", "Produces" = "produces"), 
-                  selected = 1),
-      selectInput("lang", label = h4("Language"), 
-                  choices = list("English" = 1), 
-                  selected = 1),
+      uiOutput("language_selector"),
+      uiOutput("form_selector"),      
+      uiOutput("measure_selector"),
+      
       selectInput("qsize", label = h4("Quantile size"), 
                   choices = list("10%" = .1, "20%" = .2, "25%" = .25),
                   selected = 1),
       sliderInput("age", label = h4("Age (Months)"), 
-                  min = 8, max = 36, value = 12)
+                  min = 8, max = 36, value = 24)
       ),
 
     # Show a plot of the generated distribution
       mainPanel(
+        tags$style(type="text/css",
+                   ".shiny-output-error { visibility: hidden; }",
+                   ".shiny-output-error:before { visibility: hidden; }"
+        ),
         plotOutput("plot")
       )
     )
