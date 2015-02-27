092b0c36e91b9bee2f599e05d40b288cded162f9
Mika Braginsky <mikabr@stanford.edu>
2015-02-26 18:44:22 -0800

    multiple word selection

--- shiny_apps/word_trajectory_app/ui.R
+++ shiny_apps/word_trajectory_app/ui.R
@@ -7,19 +7,10 @@ shinyUI(fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-#      selectInput("language", label = h4("Language"), 
-#                  choices = list("English" = "English"), 
-#                  selected = 1),
-#      selectInput("form", label = h4("Form"), 
-#                  choices = list("Words and Sentences" = "WS",
-#                                 "Words and Gestures" = "WG"), 
-#                  selected = 1),
-      uiOutput("language_selector_control"),
-      uiOutput("form_selector_control"),      
-      uiOutput("measure_selector_control"),
-      uiOutput("word1_selector"),
-      uiOutput("word2_selector"),
-      uiOutput("word3_selector")),
+      uiOutput("language_selector"),
+      uiOutput("form_selector"),      
+      uiOutput("measure_selector"),
+      uiOutput("words_selector")),
 
     # Show a plot of the generated distribution
       mainPanel(
