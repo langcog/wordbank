106cc71035b99bcd2f5840f578547b35ee726bcb
Mika Braginsky <mikabr@stanford.edu>
2015-03-05 22:36:45 -0800

    download plot and download data buttons in all reports

--- shiny_apps/norms_app/ui.R
+++ shiny_apps/norms_app/ui.R
@@ -13,16 +13,19 @@ shinyUI(fluidPage(
       selectInput("qsize", label = h4("Quantile size"), 
                   choices = list("10%" = .1, "20%" = .2, "25%" = .25),
                   selected = .2),
+      br(),
+      downloadButton('downloadPlot', 'Download Plot'),
+      br(),br(),
+      downloadButton('downloadData', 'Download Data'),
+#      downloadButton('downloadReport', 'Download Report'),
       width=3),
-    
+        
     # Show a plot of the generated distribution
     mainPanel(
       tags$style(type="text/css",
                  ".shiny-output-error { visibility: hidden; }",
-                 ".shiny-output-error:before { visibility: hidden; }"
-      ),
+                 ".shiny-output-error:before { visibility: hidden; }"),
       plotOutput("plot", width = "100%", height = "auto"),
-      width=9
-    )
+      width=9)
   )
 ))
