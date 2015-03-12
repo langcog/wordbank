3788dc9878dda27bc05dee0b1640a669c7990d48
Mika Braginsky <mikabr@stanford.edu>
2015-03-11 18:41:12 -0700

    first pass at longform data table report #51

---
+++ shiny_apps/instrument_data_app/ui.R
@@ -0,0 +1,25 @@
+library(shiny)
+library(shinythemes)
+
+shinyUI(fluidPage(theme = shinytheme("spacelab"),
+  
+  # Application title
+  titlePanel("Instrument Data"),
+  
+  br(),
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      uiOutput("language_selector"),
+      uiOutput("form_selector"),
+      width=3
+      ),
+    
+    mainPanel(
+      dataTableOutput(outputId="table"),
+      width=9
+    )
+    
+  )
+))
