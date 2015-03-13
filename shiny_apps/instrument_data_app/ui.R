6604e37a7de7c6c2373ab637f356a98e7a1d2949
Mika Braginsky <mikabr@stanford.edu>
2015-03-13 11:30:23 -0700

    button placement in instrument data report

--- shiny_apps/instrument_data_app/ui.R
+++ shiny_apps/instrument_data_app/ui.R
@@ -1,30 +1,33 @@
 library(shiny)
 library(shinythemes)
 
-shinyUI(fluidPage(theme = shinytheme("spacelab"),
+shinyUI(fluidPage(
+  theme = shinytheme("spacelab"),
   
-  # Application title
-  titlePanel("Instrument Data"),
-  
+  titlePanel("Instrument Data"),  
   br(),
   
   sidebarLayout(
     
-    sidebarPanel(width=3,
+    sidebarPanel(
+      width=3,
+      span(align = "center",
+           submitButton("Get Data"),
+           p("Caution: takes a while")),
       uiOutput("language_selector"),
       uiOutput("form_selector"),
-      submitButton("Get Data"),
-      br(),br(),
+      br(), br(),
       downloadButton('downloadData', 'Download Data')
-      ),
+    ),
     
-    mainPanel(width=9,
+    mainPanel(
+      width=9,
       conditionalPanel(condition="$('html').attr('class') == 'shiny-busy'",
                        fluidRow(column(12, tags$h4("Loading data..."), align="center")),
                        fluidRow(column(12, imageOutput("loading"), align="center"))
       ),
       conditionalPanel(condition="$('html').attr('class') != 'shiny-busy'",
-        dataTableOutput(outputId="table")
+                       dataTableOutput(outputId="table")
       )
     )
     
