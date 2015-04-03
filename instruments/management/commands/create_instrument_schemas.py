43ca984ac551da0475b3e2954fed7fce0a410c90
Mika Braginsky <mikabr@stanford.edu>
2015-04-02 21:03:16 -0700

    organized instrument models and added new ones generated by create_instrument_schemas #70

--- instruments/management/commands/create_instrument_schemas.py
+++ instruments/management/commands/create_instrument_schemas.py
@@ -1,4 +1,5 @@
 import xlrd
+import codecs
 import json
 from django.core.management.base import NoArgsCommand
 
@@ -7,23 +8,41 @@ class Command(NoArgsCommand):
 
     def handle(self, *args, **options):
 
-        f = open('instruments/models.py', 'a')
+        f = open('instruments/models.py', 'w')
         instruments = json.load(open('static/json/instruments.json'))
 
         for instrument in instruments:
 
             instr = '_'.join([instrument['language'], instrument['form']])
-            f.write('from %s import *\n' % (instr))
-            instrument_file = open('instruments/%s.py' % (instr), 'w')
+            f.write('from schemas.%s import *\n' % (instr))
+            instrument_file = open('instruments/schemas/%s.py' % (instr), 'w')
 
             instrument_file.write('from django.db import models\n')
             instrument_file.write('from base import BaseTable\n')
 
-            book = xlrd.open_workbook(instrument['file'])
+            ftype = instrument['file'].split('.')[-1]
 
-            sheet = book.sheet_by_index(0)
-            nrows = sheet.nrows
-            col_names = list(sheet.row_values(0))
+            if ftype == 'xlsx' or ftype == 'xls':
+                sheet = xlrd.open_workbook(instrument['file']).sheet_by_index(0)
+                col_names = list(sheet.row_values(0))
+                nrows = sheet.nrows
+                get_row = lambda row: list(sheet.row_values(row))
+            elif ftype == 'csv':
+                contents = [field for field in [line.split(',')
+                                                for line in codecs.open(instrument['file'],
+                                                                        encoding='utf-8').read().split('\n')]]
+                col_names = contents[0]
+                nrows = len(contents)
+                get_row = lambda row: contents[row]
+            else:
+                raise IOError("Instrument file must be xlsx, xls, or csv.")
+
+
+#            book = xlrd.open_workbook(instrument['file'])
+
+#            sheet = book.sheet_by_index(0)
+#            nrows = sheet.nrows
+#            col_names = list(sheet.row_values(0))
 
             instrument_file.write('\n\nclass %s(BaseTable):\n' % instr)
 
@@ -31,11 +50,13 @@ class Command(NoArgsCommand):
                 instrument_file.write('    pass\n')
 
             for row in xrange(1, nrows):
-                row_values = list(sheet.row_values(row))
-                itemID = row_values[col_names.index('itemID')]
-                choices = row_values[col_names.index('choices')].split(', ')
-                instrument_file.write('    %s_choices = %s\n' % (itemID, [(c,c) for c in choices]))
-                instrument_file.write('    %s = models.CharField(max_length=20, choices=%s_choices, null=True)\n' % (itemID, itemID))
+#                row_values = list(sheet.row_values(row))
+                row_values = get_row(row)
+                if len(row_values) > 1:
+                    itemID = row_values[col_names.index('itemID')]
+                    choices = row_values[col_names.index('choices')].split('; ')
+                    instrument_file.write('    %s_choices = %s\n' % (itemID, [(c,c) for c in choices]))
+                    instrument_file.write('    %s = models.CharField(max_length=20, choices=%s_choices, null=True)\n' % (itemID, itemID))
 
             instrument_file.close()
 
