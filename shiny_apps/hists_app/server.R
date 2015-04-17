a751e2ad62141a57b19b3e5fcb25539d76941a36
Mika Braginsky <mikabr@stanford.edu>
2015-04-16 20:39:34 -0700

    deleted extraneous host in hists app server

--- shiny_apps/hists_app/server.R
+++ shiny_apps/hists_app/server.R
@@ -18,7 +18,7 @@ shinyServer(function(input, output, session) {
   output$loaded <- reactive({0})
   outputOptions(output, 'loaded', suspendWhenHidden=FALSE)
   
-  wordbank <- src_mysql(dbname = "wordbank", user = "wordbank", host = "54.200.225.86",
+  wordbank <- src_mysql(dbname = "wordbank", user = "wordbank",
                         password = "wordbank")
   
   common.tables <- get.common.tables(wordbank)
