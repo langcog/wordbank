ebcde29f003431ed335cd378eca66582cf76995b
Mika Braginsky <mikabr@stanford.edu>
2015-04-16 19:05:56 -0700

    added birth order to stats aggregation

--- instruments/aggregate_stats_helper.py
+++ instruments/aggregate_stats_helper.py
@@ -26,9 +26,17 @@ def aggregate(admin_query=None):
         else:
             momed_obj = MomEd.objects.get(pk=administration.child.momed_id)
             momed = str(momed_obj.order) + '. ' + momed_obj.level
+        if administration.child.birth_order is None:
+            birth_order = None
+        else:
+            get_birth_order = lambda bo: {1: 'First',
+                                          2: 'Second',
+                                          3: 'Third'}.get(bo, 'Fourth or more')
+            birth_order = get_birth_order(administration.child.birth_order)
         obj = {'age': safe_field(administration.age),
                'sex': safe_field(administration.child.get_sex_display()),
                'mom_ed': safe_field(momed),
+               'birth_order': safe_field(birth_order),
                'source': safe_field(administration.source.name),
                'ethnicity': safe_field(administration.child.get_ethnicity_display())}
         instrument_language = administration.instrument.language
