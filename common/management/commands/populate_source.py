901f4eea698c99b601101de0ddce016d1a60b5bc
Mika Braginsky <mikabr@stanford.edu>
2015-04-22 23:53:22 -0700

    added contributor and citation fields

--- common/management/commands/populate_source.py
+++ common/management/commands/populate_source.py
@@ -10,11 +10,21 @@ class Command(NoArgsCommand):
         sources = json.load(open('static/json/datasets.json'))
 
         for source in sources:
-            if not Source.objects.filter(name=source['name'],
-                                         dataset=source['dataset'],
-                                         instrument_language=source['instrument_language'],
-                                         instrument_form=source['instrument_form']).exists():
+            if Source.objects.filter(name=source['name'],
+                                     dataset=source['dataset'],
+                                     instrument_language=source['instrument_language'],
+                                     instrument_form=source['instrument_form']).exists():
+                source_obj = Source.objects.get(name=source['name'],
+                                                dataset=source['dataset'],
+                                                instrument_language=source['instrument_language'],
+                                                instrument_form=source['instrument_form'])
+                source_obj.contributor = source['contributor']
+                source_obj.citation = source['citation']
+                source_obj.save()
+            else:
                 Source.objects.create(name=source['name'],
                                       dataset=source['dataset'],
                                       instrument_language=source['instrument_language'],
-                                      instrument_form=source['instrument_form'])
+                                      instrument_form=source['instrument_form'],
+                                      contributor = source['contributor'],
+                                      citation = source['citation'])
