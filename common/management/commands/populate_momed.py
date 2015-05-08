33cfd4ebe96d97b40a5b4ae84e8d2a3162122657
Mika Braginsky <mikabr@stanford.edu>
2015-05-08 10:58:13 -0700

    updated all commands for django 1.8 arg parsing

--- common/management/commands/populate_momed.py
+++ common/management/commands/populate_momed.py
@@ -1,8 +1,8 @@
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 from common.models import *
 
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
