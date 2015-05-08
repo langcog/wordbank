d28084286aa8056809b0172d99c0720960970016
Mika Braginsky <mikabr@stanford.edu>
2015-05-07 23:09:06 -0700

    gave up and re-did common migrations

--- common/migrations/0001_initial.py
+++ common/migrations/0001_initial.py
@@ -1,4 +1,4 @@
-# encoding: utf8
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models, migrations
@@ -11,40 +11,78 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='InstrumentsMap',
+            name='Administration',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=20)),
-                ('language', models.CharField(max_length=20)),
+                ('date_of_test', models.DateField(null=True, blank=True)),
+                ('data_id', models.IntegerField()),
+                ('age', models.IntegerField()),
+                ('data_age', models.IntegerField(null=True, blank=True)),
+                ('production', models.IntegerField(null=True)),
+                ('comprehension', models.IntegerField(null=True)),
             ],
-            options={
-            },
-            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50)),
+                ('lexical_category', models.CharField(max_length=20)),
+            ],
         ),
         migrations.CreateModel(
             name='Child',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('study_id', models.CharField(max_length=20)),
                 ('birth_order', models.IntegerField(null=True, blank=True)),
                 ('date_of_birth', models.DateField(null=True, blank=True)),
-                ('ethnicity', models.CharField(blank=True, max_length=1, null=True, choices=[(b'A', b'Asian'), (b'B', b'Black'), (b'H', b'Hispanic'), (b'W', b'White')])),
-                ('mom_ed', models.IntegerField(null=True, blank=True)),
-                ('sex', models.CharField(blank=True, max_length=1, null=True, choices=[(b'M', b'Male'), (b'F', b'Female')])),
-                ('study_id', models.CharField(max_length=20)),
+                ('ethnicity', models.CharField(blank=True, max_length=1, null=True, choices=[(b'A', b'Asian'), (b'B', b'Black'), (b'H', b'Hispanic'), (b'W', b'White'), (b'O', b'Other/Mixed')])),
+                ('study_momed', models.CharField(max_length=100, null=True, blank=True)),
+                ('sex', models.CharField(blank=True, max_length=1, null=True, choices=[(b'M', b'Male'), (b'F', b'Female'), (b'O', b'Other')])),
             ],
-            options={
-            },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
-            name='Ethnicity',
+            name='Instrument',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('ethnicity', models.CharField(max_length=30)),
+                ('language', models.CharField(max_length=30)),
+                ('form', models.CharField(max_length=2, choices=[(b'WS', b'Words & Sentences'), (b'WG', b'Words & Gestures'), (b'TC', b'Toddler Checklist'), (b'IC', b'Infant Checklist')])),
+                ('age_min', models.IntegerField()),
+                ('age_max', models.IntegerField()),
+                ('has_grammar', models.BooleanField(default=False)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='ItemInfo',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('item', models.CharField(max_length=50)),
+                ('item_id', models.CharField(max_length=20)),
+                ('type', models.CharField(max_length=30)),
+                ('definition', models.CharField(max_length=200, null=True, blank=True)),
+                ('gloss', models.CharField(max_length=80, null=True, blank=True)),
+                ('complexity_category', models.CharField(max_length=30, null=True, blank=True)),
+                ('category', models.ForeignKey(to='common.Category', null=True)),
+                ('instrument', models.ForeignKey(to='common.Instrument')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='ItemMap',
+            fields=[
+                ('uni_lemma', models.CharField(max_length=50, serialize=False, primary_key=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='MomEd',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('level', models.CharField(max_length=20, null=True, blank=True)),
+                ('order', models.IntegerField(unique=True)),
             ],
             options={
+                'ordering': ['order'],
             },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Source',
@@ -52,51 +90,35 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=20)),
                 ('dataset', models.CharField(max_length=20, null=True, blank=True)),
-                ('instrument', models.CharField(max_length=20, null=True, blank=True)),
-                ('citation', models.CharField(max_length=50)),
+                ('instrument_language', models.CharField(max_length=30)),
+                ('instrument_form', models.CharField(max_length=20)),
+                ('contributor', models.TextField(blank=True)),
+                ('citation', models.TextField(blank=True)),
             ],
-            options={
-            },
-            bases=(models.Model,),
         ),
-        migrations.CreateModel(
-            name='WordInfo',
-            fields=[
-                ('uni_lemma', models.CharField(max_length=20, serialize=False, primary_key=True)),
-                ('lang_lemma', models.CharField(max_length=20)),
-            ],
-            options={
-            },
-            bases=(models.Model,),
+        migrations.AddField(
+            model_name='iteminfo',
+            name='map',
+            field=models.ForeignKey(blank=True, to='common.ItemMap', null=True),
         ),
-        migrations.CreateModel(
-            name='Administration',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('child', models.ForeignKey(to='common.Child', to_field='id')),
-                ('instrument', models.ForeignKey(to='common.InstrumentsMap', to_field='id')),
-                ('source', models.ForeignKey(to_field='id', blank=True, to='common.Source', null=True)),
-                ('date_of_test', models.DateField(null=True, blank=True)),
-                ('data_id', models.IntegerField()),
-                ('age', models.IntegerField()),
-                ('data_age', models.IntegerField(null=True, blank=True)),
-            ],
-            options={
-            },
-            bases=(models.Model,),
+        migrations.AddField(
+            model_name='child',
+            name='momed',
+            field=models.ForeignKey(blank=True, to='common.MomEd', null=True),
         ),
-        migrations.CreateModel(
-            name='WordMapping',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('column', models.CharField(max_length=8)),
-                ('instrument', models.ForeignKey(to='common.InstrumentsMap', to_field='id')),
-                ('category', models.CharField(max_length=20, null=True, blank=True)),
-                ('definition', models.CharField(max_length=100)),
-                ('word_info', models.ForeignKey(to='common.WordInfo', to_field=b'uni_lemma')),
-            ],
-            options={
-            },
-            bases=(models.Model,),
+        migrations.AddField(
+            model_name='administration',
+            name='child',
+            field=models.ForeignKey(to='common.Child'),
+        ),
+        migrations.AddField(
+            model_name='administration',
+            name='instrument',
+            field=models.ForeignKey(to='common.Instrument'),
+        ),
+        migrations.AddField(
+            model_name='administration',
+            name='source',
+            field=models.ForeignKey(blank=True, to='common.Source', null=True),
         ),
     ]
