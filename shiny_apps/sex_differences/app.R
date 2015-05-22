b4ca0c855719f5a379b56a154317fd43970f5fe0
Mika Braginsky <mikabr@stanford.edu>
2015-05-21 22:50:40 -0700

    renamed shiny apps and added static report shiny apps

--- shiny_apps/sex_differences/app.R
+++ shiny_apps/sex_differences/app.R
@@ -1,3 +1,3 @@
 server <- function(input, output) {}
-ui <- shinyUI(includeHTML("../../Ranalysis/sex_differences/sex_differences.html"))
+ui <- shinyUI(includeHTML("https://rawgit.com/langcog/wordbank/master/Ranalysis/sex_differences/sex_differences.html"))
 shinyApp(ui = ui, server = server)
