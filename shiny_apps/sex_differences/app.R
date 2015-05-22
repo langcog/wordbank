f0f40637ce2132e5e98336031c037a861e9c6662
Mika Braginsky <mikabr@stanford.edu>
2015-05-21 23:35:31 -0700

    trying to use Rmd files for static reports

--- shiny_apps/sex_differences/app.R
+++
@@ -1,3 +0,0 @@
-server <- function(input, output) {}
-ui <- shinyUI(includeHTML("https://rawgit.com/langcog/wordbank/master/Ranalysis/sex_differences/sex_differences.html"))
-shinyApp(ui = ui, server = server)
