849af942319069a9b78793ea7b5bf026fbaa1b6b
Mika Braginsky <mikabr@stanford.edu>
2015-06-04 16:21:55 -0700

    text resizing and color palette iss99

--- static/js/home.js
+++ static/js/home.js
@@ -1,16 +1,16 @@
 var diameter = 500,
     format = d3.format(",d"),
-    color = d3.scale.ordinal().range(['#268bd2', '#cb4b16', '#859900', '#993399', '#d33682', '#b58900', '#2aa198', '#6c71c4', '#dc322f']);
+    color = d3.scale.category20c();//ordinal().range(['#268bd2', '#cb4b16', '#859900', '#993399', '#d33682', '#b58900', '#2aa198', '#6c71c4', '#dc322f']);
     //['#d33682', '#dc322f', '#cb4b16', '#b58900', '#859900', '#2aa198', '#268bd2', '#6c71c4', '#993399']
 
 var bubble = d3.layout.pack()
     .sort(null)
-    .size([diameter, 500])
-    .padding(7);
+    .size([diameter, diameter])
+    .padding(2);
 
 var svg = d3.select("#langStatsChart").append("svg")
     .attr("width", diameter)
-    .attr("height", 500)
+    .attr("height", diameter)
     .attr("class", "bubble");
 
 var tip = d3.tip()
@@ -24,6 +24,7 @@ svg.call(tip);
 
 d3.json("static/json/langStats.json", function(error, root) {
 
+  console.log(classes(root));
   var node = svg.selectAll(".node")
       .data(bubble.nodes(classes(root))
       .filter(function(d) { return !d.children; }))
@@ -36,13 +37,15 @@ d3.json("static/json/langStats.json", function(error, root) {
       .text(function(d) { return d.name + ": " + format(d.value); });
 
   node.append("circle")
-      .attr("r", function(d) { return d.r; })
+      .attr("r", function(d) { return d.r ; })
       .style("fill", function(d) { return color(d.name); });
 
   node.append("text")
-      .attr("dy", ".3em")
+      .attr("dy", ".35em")
       .style("text-anchor", "middle")
-      .text(function(d) { return d.name.substring(0, d.r / 3); })
+//      .text(function(d) { return d.name.substring(0, d.r / 3); })
+      .text(function(d) { return d.name; })
+      .style("font-size", function(d) { return Math.min(2 * d.r, (2 * d.r - 4) / this.getComputedTextLength() * 12) + "px"; })
       .on('mouseover', tip.show)
       .on('mouseout', tip.hide);
 
