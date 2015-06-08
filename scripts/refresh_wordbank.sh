0b5d80fcfd9769f529d572f6f4874de08e3db52b
Mika Braginsky <mikabr@stanford.edu>
2015-06-08 13:55:28 -0700

    suppressed langStats outfile and data saving

--- scripts/refresh_wordbank.sh
+++ scripts/refresh_wordbank.sh
@@ -2,5 +2,5 @@ cd /home/ubuntu/wordbank
 git pull origin master
 # bash /home/ubuntu/wordbank/scripts/reset_tables.sh
 python /home/ubuntu/wordbank/manage.py aggregate_stats
-R CMD BATCH /home/ubuntu/wordbank/scripts/langStats.R
+R CMD BATCH --no-save --no-restore /home/ubuntu/wordbank/scripts/langStats.R /dev/null
 sudo apachectl -k graceful
