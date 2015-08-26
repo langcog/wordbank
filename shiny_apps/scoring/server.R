c74641ef428c81366e67cdfaaa86d30fb868b595
Michael C. Frank <mcfrank@stanford.edu>
2015-08-26 15:16:36 -0700

    added download link

--- shiny_apps/scoring/server.R
+++ shiny_apps/scoring/server.R
@@ -1,27 +1,38 @@
 library(shiny)
 library(dplyr)
+library(readr)
 
-lookup <- read.csv("lookup.csv")
+lookup <- read_csv("lookup.csv")
+
+# input$file1 will be NULL initially. After the user selects
+# and uploads a file, it will be a data frame with 'name',
+# 'size', 'type', and 'datapath' columns. The 'datapath'
+# column will contain the local filenames where the data can
+# be found.
 
 shinyServer(function(input, output) {
-  output$contents <- renderTable({
-    
-    # input$file1 will be NULL initially. After the user selects
-    # and uploads a file, it will be a data frame with 'name',
-    # 'size', 'type', and 'datapath' columns. The 'datapath'
-    # column will contain the local filenames where the data can
-    # be found.
-    
+  data <- reactive({
     inFile <- input$file1
     
     if (is.null(inFile))
       return(NULL)
     
     d <- read.csv(inFile$datapath, header=input$header, sep=input$sep, 
-             quote=input$quote)
+                  quote=input$quote)
     
-    d <- left_join(d, lookup)
-    
-    d
+    left_join(d, lookup)
   })
+  
+  output$contents <- renderTable({
+    data()
+  })
+  
+  output$downloadData <- downloadHandler(
+    filename = function() { 
+      paste0("percentiles.csv") 
+    },
+    content = function(file) {
+      write_csv(data(), file)
+    }
+  )
 })
