f63f058c8248d1be294aa3fab1b8d0346e77abbb
Mika Braginsky <mikabr@stanford.edu>
2015-09-16 14:26:21 -0700

    removed old stats stuff, updated langStats script to use wordbankr

--- scripts/langStats.R
+++ scripts/langStats.R
@@ -1,18 +1,14 @@
 library(dplyr)
 library(jsonlite)
-source("/home/ubuntu/wordbank/shiny_apps/data_loading.R")
+library(wordbankr)
 
-wordbank <- connect.to.wordbank("prod")
+admins <- get_administration_data(mode = "local")
 
-common.tables <- get.common.tables(wordbank)
-
-admins <- get.administration.data(common.tables)
-
-lang.summary <- admins %>%
+lang_summary <- admins %>%
   group_by(language) %>%
   summarise(count = length(unique(data_id))) %>%
   rename(name = language)
 
-lang.json <- toJSON(lang.summary, pretty = TRUE)
+lang_json <- toJSON(lang_summary, pretty = TRUE)
 
-write(paste0('{"name": "", "children":\n', lang.json, '}'), "/home/ubuntu/wordbank/static/json/langStats.json")
+write(paste0('{"name": "", "children":\n', lang_json, '}'), "/home/ubuntu/wordbank/static/json/langStats.json")
--- scripts/refresh_wordbank.sh
+++ scripts/refresh_wordbank.sh
@@ -1,6 +1,4 @@
 cd /home/ubuntu/wordbank
 git pull origin master
-# bash /home/ubuntu/wordbank/scripts/reset_tables.sh
-python /home/ubuntu/wordbank/manage.py aggregate_stats
 R CMD BATCH --no-save --no-restore /home/ubuntu/wordbank/scripts/langStats.R /dev/null
 sudo apachectl -k graceful
