ae312ee2f5f8d1232bcc9736a8c132bd7603e20c
Mika Braginsky <mikabr@stanford.edu>
2015-09-16 01:28:04 -0700

    imported Italian WG

---
+++ common/migrations/0005_auto_20150916_0247.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('common', '0004_category_lexical_class'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='child',
+            name='study_id',
+            field=models.CharField(max_length=20, null=True),
+        ),
+    ]
