44afe33d8bfa488b5ceb98d62edc3768d3735d6d
Mika Braginsky <mikabr@stanford.edu>
2015-10-13 15:58:27 -0700

    got rid of outdated stuff

--- shiny_apps/ses_effects/app.R
+++
@@ -1,6 +0,0 @@
-server <- function(input, output) {}
-ui <- shinyUI(
-	renderText("This report is not available yet.")
-	#includeHTML("../../Ranalysis/ses_effects/ses_effects.html")
-	)
-shinyApp(ui = ui, server = server)
