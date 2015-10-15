a2972511bb019d7f15f97548e5b3762ee92aa55a
Mika Braginsky <mikabr@stanford.edu>
2015-10-15 16:50:23 -0700

    restored missing stats stuff

--- scripts/refresh_wordbank.sh
+++ scripts/refresh_wordbank.sh
@@ -1,4 +1,5 @@
 cd /home/ubuntu/wordbank
 git pull origin master
+python /home/ubuntu/wordbank/manage.py aggregate_stats
 R CMD BATCH --no-save --no-restore /home/ubuntu/wordbank/scripts/langStats.R /dev/null
 sudo apachectl -k graceful
