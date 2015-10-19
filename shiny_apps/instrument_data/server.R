64d786ca1d2fd7ddd61f2917511582706fe17df4
Mika Braginsky <mikabr@stanford.edu>
2015-10-19 13:49:19 -0700

    updated instrument_data app to use wordbankr

--- shiny_apps/instrument_data/server.R
+++ shiny_apps/instrument_data/server.R
@@ -1,71 +1,51 @@
 library(shiny)
-library(RMySQL)
 library(dplyr)
 library(magrittr)
-source("../data_loading.R")
+library(wordbankr)
+library()
 
 ## DEBUGGING
 input <- list(language = "English", form = "WS", age = c(18,30))
 
 shinyServer(function(input, output, session) {
   
-  output$loaded <- reactive({0})
-  outputOptions(output, 'loaded', suspendWhenHidden=FALSE)
+  output$loaded <- reactive(0)
+  outputOptions(output, 'loaded', suspendWhenHidden = FALSE)
   
-  wordbank <- connect.to.wordbank("local")
+  admins <- get_administration_data(mode = "local") %>%
+    select(data_id, language, form, age, sex, momed, comprehension, production) %>%
+    mutate(sex = factor(sex, levels = c("F", "M", "O"),
+                        labels = c("Female", "Male", "Other")))
   
-  common.tables <- get.common.tables(wordbank)
+  items <- get_item_data() %>%
+    select(num_item_id, item_id, type, category, definition, language, form)
   
-  admins <- get.administration.data(common.tables) %>%
-    select(data_id, language, form, age, sex, momed.level, comprehension, production) %>%
-    rename(momed = momed.level) %>%
-    mutate(sex = factor(sex, levels=c("F", "M", "O"), labels=c("Female", "Male", "Other")))
-  
-  items <- get.item.data(common.tables) %>%
-    select(item.id, type, category, definition, language, form) %>%
-    mutate(item.id = as.numeric(substr(item.id, 6, nchar(item.id))))
-  
-  instrument.tables <- get.instrument.tables(wordbank, common.tables)
-  languages <- sort(unique(instrument.tables$language))
-  #forms <- unique(instrument.tables$form)
-  
-  start.language <- function() {"English"}
-  start.form <- function() {"WS"}
+  languages <- sort(unique(items$language))
+
+  start_language <- function() "English"
+  start_form <- function() "WS"
   
   
-  input.language <- reactive({
-    ifelse(is.null(input$language), start.language(), input$language)
+  input_language <- reactive({
+    ifelse(is.null(input$language), start_language(), input$language)
   })
   
-  input.form <- reactive({
-    ifelse(is.null(input$form), start.form(), input$form)
-  })
-  
-#  input.language <- reactive({input$language})
-  
-#  input.form <- reactive({input$form})
-  
-  instrument <- reactive({
-    if(!is.null(input.language()) & !is.null(input.form())) {
-      filter(instrument.tables,
-             language == input.language(),
-             form == input.form())
-    }
+  input_form <- reactive({
+    ifelse(is.null(input$form), start_form(), input$form)
   })
   
   data <- eventReactive(input$get_data, {
     
-    if(!is.null(input.language()) & !is.null(input.form())) {
+    if(!is.null(input_language()) & !is.null(input_form())) {
       
-      instrument.items <- filter(items, language == input.language(), form == input.form()) %>%
+      instrument_items <- items %>%
+        filter(language == input_language(), form == input_form()) %>%
         select(-language, -form)
       
-      instrument.table <- instrument()$table[[1]]
-      fields <- as.character(instrument.table$select[2:length(instrument.table$select)])
-      
-      get.instrument.data(instrument.table, fields) %>%
-        left_join(instrument.items) %>%
-        left_join(select(admins, -language, -form, -comprehension, -production)) %>%
+      get_instrument_data(input_language(), input_form(), items = NULL,
+                          iteminfo = instrument_items,
+                          administrations = select(admins, -language, -form, -comprehension, -production)) %>%
+        select(-num_item_id) %>%
         arrange(data_id)
     }
   })
@@ -76,18 +56,18 @@ shinyServer(function(input, output, session) {
   
   output$language_selector <- renderUI({    
     selectizeInput("language", label = h4("Language"),
-                   choices = languages, selected = start.language())
+                   choices = languages, selected = start_language())
   })
   
   forms <- reactive({
-    Filter(function(form) {form %in% unique(filter(instrument.tables,
-                                                   language == input.language())$form)},
+    Filter(function(form) {form %in% unique(filter(items,
+                                                   language == input_language())$form)},
            list("Words & Sentences" = "WS", "Words & Gestures" = "WG"))
   })
   
   output$form_selector <- renderUI({    
     selectizeInput("form", label = h4("Form"),
-                   choices = forms(), selected = start.form())
+                   choices = forms(), selected = start_form())
   })
   
   output$downloadData <- downloadHandler(
