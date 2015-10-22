037285614a96a0e18ded0d0f2c0739e6205039e7
Mika Braginsky <mikabr@stanford.edu>
2015-10-21 23:59:17 -0700

    shiny apps updated for wordbankr, linted

--- shiny_apps/instrument_data/ui.R
+++ shiny_apps/instrument_data/ui.R
@@ -3,41 +3,43 @@ library(shinythemes)
 
 shinyUI(fluidPage(
   theme = shinytheme("spacelab"),
-  
-  titlePanel("Instrument Data"),  
+
+  titlePanel("Instrument Data"),
   br(),
-  
+
   sidebarLayout(
-    
+
     sidebarPanel(
-      width=3,
-      
+      width = 3,
+
       conditionalPanel(
-        condition="output.loaded != 1",
+        condition = "output.loaded != 1",
         h4("Loading...")
       ),
-      
+
       conditionalPanel(
-        condition="output.loaded == 1",
-        
+        condition = "output.loaded == 1",
+
         actionButton("get_data", "Get Data"),
-        p("Caution: takes a while"),
+        p("Caution: can take a while"),
         uiOutput("language_selector"),
         uiOutput("form_selector"),
         br(), br(),
-        downloadButton('downloadData', 'Download Data')
+        downloadButton("download_data", "Download Data")
       )),
-    
+
     mainPanel(
-      width=9,
-      conditionalPanel(condition="$('html').attr('class') == 'shiny-busy'",
-                       fluidRow(column(12, tags$h4("Please wait..."), align="center")),
-                       fluidRow(column(12, imageOutput("loading"), align="center"))
+      width = 9,
+      conditionalPanel(condition = "$('html').attr('class') == 'shiny-busy'",
+                       fluidRow(column(12, tags$h4("Please wait..."),
+                                       align = "center")),
+                       fluidRow(column(12, imageOutput("loading"),
+                                       align = "center"))
       ),
-      conditionalPanel(condition="$('html').attr('class') != 'shiny-busy'",
-                       dataTableOutput(outputId="table")
+      conditionalPanel(condition = "$('html').attr('class') != 'shiny-busy'",
+                       dataTableOutput(outputId = "table")
       )
     )
-    
+
   )
 ))
--- shiny_apps/item_data/ui.R
+++ shiny_apps/item_data/ui.R
@@ -3,52 +3,49 @@ library(shinythemes)
 
 shinyUI(fluidPage(
   theme = shinytheme("spacelab"),
-  
-  titlePanel("Item Data"),  
+
+  titlePanel("Item Data"),
   br(),
-  
+
   sidebarLayout(
-    
+
     sidebarPanel(
-      width=3,
-      
+      width = 3,
+
       conditionalPanel(
-        condition="output.loaded != 1",
+        condition = "output.loaded != 1",
         h4("Loading...")
       ),
-      
+
       conditionalPanel(
-        condition="output.loaded == 1",
-        
+        condition = "output.loaded == 1",
+
         uiOutput("language_selector"),
         uiOutput("form_selector"),
         uiOutput("measure_selector"),
         uiOutput("age_selector"),
         br(), br(),
-        downloadButton('downloadAll', 'Download all pages', class = "btn-primary btn-sm"),
+        downloadButton("download_all", "Download all pages",
+                       class = "btn-primary btn-sm"),
         br(), br(),
-        downloadButton('downloadCurrent', 'Download current page', class = "btn-primary btn-sm"),
+        downloadButton("download_current", "Download current page",
+                       class = "btn-primary btn-sm"),
         br(), br(),
-        downloadButton('downloadSelected', 'Download selected rows', class = "btn-primary btn-sm")
+        downloadButton("download_selected", "Download selected rows",
+                       class = "btn-primary btn-sm")
       )
     ),
-    
+
     mainPanel(
-      width=9,
+      width = 9,
       tags$style(type = "text/css",
                  ".shiny-output-error { visibility: hidden; }",
-                 ".shiny-output-error:before { visibility: hidden; }"),      
+                 ".shiny-output-error:before { visibility: hidden; }"),
       conditionalPanel(
-        condition="output.loaded == 1",
-#     conditionalPanel(condition="$('html').attr('class') == 'shiny-busy'",
-#                      fluidRow(column(12, tags$h4("Please wait..."), align="center")),
-#                      fluidRow(column(12, imageOutput("loading"), align="center"))
-#     ),
-#     conditionalPanel(condition="$('html').attr('class') != 'shiny-busy'",
-                       DT::dataTableOutput('table')
-#      )
-        )
+        condition = "output.loaded == 1",
+        DT::dataTableOutput("table")
       )
-    
+    )
+
   )
 ))
