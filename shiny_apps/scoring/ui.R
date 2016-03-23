aff2afbb22d3df3f5d9876a15f15388f866a910b
Michael C. Frank <mcfrank@stanford.edu>
2016-03-23 16:11:04 -0700

    almost working prototype

--- shiny_apps/scoring/ui.R
+++ shiny_apps/scoring/ui.R
@@ -1,30 +1,56 @@
 library(shiny)
+library(shinythemes)
+library(shinyBS)
+
 
 shinyUI(fluidPage(
-  titlePanel("Uploading Files"),
+  theme = shinytheme("spacelab"),
+  
+  br(),
+  bsCollapse(id = "doc", open = "title",
+             bsCollapsePanel(
+               title = h3("CDI Administration Scoring"),
+               "This app is a scoring tool for MB-CDI administrations, using the Fenson et al. (2007) revised norming data.",
+               # "To use it, upload a tab- or comma-separated file with a column indicating the number of words the child knows.",
+               value = "title",
+               style = "default")),
+
   sidebarLayout(
     sidebarPanel(
-      fileInput('file1', 'Choose CSV File',
+      h4("1. Download a sample template"),
+      downloadButton('downloadSample', 'Download'),
+      tags$hr(),
+      h4("2. Upload your own data"), 
+      fileInput('file1', 'Choose file',
                 accept=c('text/csv', 
                          'text/comma-separated-values,text/plain', 
                          '.csv')),
       tags$hr(),
-      checkboxInput('header', 'Header', TRUE),
-      radioButtons('sep', 'Separator',
-                   c(Comma=',',
-                     Semicolon=';',
-                     Tab='\t'),
-                   ','),
-      radioButtons('quote', 'Quote',
-                   c(None='',
-                     'Double Quote'='"',
-                     'Single Quote'="'"),
-                   '"'), 
+      h4("3. Select the instrument to use"), 
+      selectInput('instrument', label=NULL, 
+                  choices = c("Words & Sentences (English)" = "Words & Sentences (English)")),
       tags$hr(),
-      downloadButton('downloadData', 'Download')
+      h4("4. Verify file formatting"), 
+      # checkboxInput('header', 'Header', TRUE),
+      radioButtons('sep', label=NULL, inline=TRUE,
+                   c("Comma delimited" = ',',
+                     "Tab delimited"='\t'),
+                   ','),
+      radioButtons('quote', label=NULL, inline=TRUE,
+                   c("No quotes"='',
+                     'Double quotes'='"',
+                     'Single quotes'="'"),
+                   ''), 
+      uiOutput('ageColumn'),
+      uiOutput('nwordsColumn'),
+      uiOutput('genderColumn'),
+      uiOutput('genderMappings')
     ),
     mainPanel(
-      tableOutput('contents')
+      uiOutput('tableTitle'),
+      uiOutput('download'), 
+      br(),
+      dataTableOutput('contents')
     )
   )
 ))
