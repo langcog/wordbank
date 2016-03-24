b4db92d1431ed31c313ce5f1aa053c91793ce7fc
dyurovsky <dyurovsky@gmail.com>
2016-03-23 21:50:11 -0700

    Norwegian unduping

--- raw_data/undupe.R
+++ raw_data/undupe.R
@@ -4,18 +4,19 @@ library(purrr)
 library(tidyr)
 library(readr)
 
-LANGUAGE <- "English"
+LANGUAGE <- "Norwegian"
 FORM <- "WG"
 
-norming_file <- "Marchman_Norming"
-other_files <- c("Thal_13", "Thal_16")
+norming_file <- "Kristoffersen"
+other_files <- c("Kristoffersen_longitudinal")
 
 
 read_data <- function(file) {
   data <- fread(paste0(LANGUAGE, "_", FORM, "/", LANGUAGE, FORM, "_", file, 
                "_data_original.csv")) %>%
-    filter(!is.na(ParticipantId), ParticipantId != "") %>%
-    unite(id_age, ParticipantId, CDIAge, remove = FALSE)
+  #  filter(!is.na(ParticipantId), ParticipantId != "") %>% #English
+    filter(!is.na(ID_CDI), ID_CDI != "")# %>%
+   # unite(id_age, ParticipantId, CDIAge, remove = FALSE)
   
   data$file = file
   
@@ -34,20 +35,25 @@ write_data <- function(df) {
 norming_data <- read_data(norming_file)
 
 norming_ids <- norming_data %>%
-  select(id_age) %>%
+ # select(id_age) %>%
+  select(ID_CDI) %>%
   distinct()
 
 other_data <- map(other_files, read_data) 
 
 other_ids <- other_data %>%
-  map(function(df) select(df, id_age)) %>%
+#  map(function(df) select(df, id_age)) %>%
+  map(function(df) select(df, ID_CDI)) %>%
   bind_rows()
 
 duplicated_ids <- intersect(norming_ids, other_ids)
 
 fixed_norming <- norming_data %>%
-  filter(!id_age %in% duplicated_ids$id_age) %>%
-  select(-id_age) 
+  # filter(!id_age %in% duplicated_ids$id_age) %>%
+  # select(-id_age) 
+  filter(!ID_CDI %in% duplicated_ids$ID_CDI) #%>%
+  #select(-id_age) 
+  
 
 fixed_norming$norming = TRUE
 
@@ -55,9 +61,12 @@ write_data(fixed_norming)
 
 fix_other_data <- function(df) {
   
-  df[, "norming"] = df[,id_age %in% duplicated_ids$id_age]
+  df[, "norming"] = df[,ID_CDI %in% duplicated_ids$ID_CDI]
   
-  select(df, -id_age)
+  return(df)
+#  df[, "norming"] = df[,id_age %in% duplicated_ids$id_age]
+  
+#  select(df, -id_age)
 }
 
 fixed_other <- other_data %>%
