9658fa823a9a3fd2d045de5c543bd1d2e45620db
Mika Braginsky <mikabr@stanford.edu>
2016-05-17 11:21:26 -0700

    import complexity items for Mandarin WS

---
+++ instruments/migrations/0014_auto_20160517_1301.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('instruments', '0013_auto_20160425_1940'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_810',
+            field=models.CharField(max_length=1, null=True, choices=[('3', '3'), ('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_811',
+            field=models.CharField(max_length=1, null=True, choices=[('3', '3'), ('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_812',
+            field=models.CharField(max_length=1, null=True, choices=[('4', '4'), ('3', '3'), ('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_813',
+            field=models.CharField(max_length=1, null=True, choices=[('3', '3'), ('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_814',
+            field=models.CharField(max_length=1, null=True, choices=[('4', '4'), ('3', '3'), ('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_815',
+            field=models.CharField(max_length=1, null=True, choices=[('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_816',
+            field=models.CharField(max_length=1, null=True, choices=[('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_817',
+            field=models.CharField(max_length=1, null=True, choices=[('4', '4'), ('3', '3'), ('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_818',
+            field=models.CharField(max_length=1, null=True, choices=[('5', '5'), ('4', '4'), ('3', '3'), ('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_819',
+            field=models.CharField(max_length=1, null=True, choices=[('3', '3'), ('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_820',
+            field=models.CharField(max_length=1, null=True, choices=[('3', '3'), ('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_821',
+            field=models.CharField(max_length=1, null=True, choices=[('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_822',
+            field=models.CharField(max_length=1, null=True, choices=[('4', '4'), ('3', '3'), ('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_823',
+            field=models.CharField(max_length=1, null=True, choices=[('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_824',
+            field=models.CharField(max_length=1, null=True, choices=[('3', '3'), ('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_825',
+            field=models.CharField(max_length=1, null=True, choices=[('3', '3'), ('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_826',
+            field=models.CharField(max_length=1, null=True, choices=[('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_827',
+            field=models.CharField(max_length=1, null=True, choices=[('3', '3'), ('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_828',
+            field=models.CharField(max_length=1, null=True, choices=[('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_829',
+            field=models.CharField(max_length=1, null=True, choices=[('3', '3'), ('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_830',
+            field=models.CharField(max_length=1, null=True, choices=[('3', '3'), ('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_831',
+            field=models.CharField(max_length=1, null=True, choices=[('4', '4'), ('3', '3'), ('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_832',
+            field=models.CharField(max_length=1, null=True, choices=[('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_833',
+            field=models.CharField(max_length=1, null=True, choices=[('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_834',
+            field=models.CharField(max_length=1, null=True, choices=[('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_835',
+            field=models.CharField(max_length=1, null=True, choices=[('5', '5'), ('4', '4'), ('3', '3'), ('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+        migrations.AddField(
+            model_name='mandarin_ws',
+            name='item_836',
+            field=models.CharField(max_length=1, null=True, choices=[('3', '3'), ('2', '2'), ('1', '1'), ('0', '0')]),
+        ),
+    ]
