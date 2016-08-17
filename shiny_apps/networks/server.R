62d751cd9a41f8e1796318a5655abf17f2277eef
dyurovsky <dyurovsky@gmail.com>
2016-08-17 16:23:26 -0500

    word2vec in the networks app

--- shiny_apps/networks/server.R
+++ shiny_apps/networks/server.R
@@ -19,6 +19,8 @@ font <- theme_mikabr()$text$family
 c_assocs <- read_csv(file = "assocs/c_assoc_mat.csv") 
 p_assocs <- read_csv(file = "assocs/p_assoc_mat.csv")
 all_assocs <- read_csv(file = "assocs/assoc_mat.csv")
+w2v_assocs<- read_csv(file= 'assocs/w2v_assocs.csv')
+# pb_assocs<- read_csv(file = 'assocs/books_word2vec.csv')
 
 ws_aoas <- read_csv("aoas/eng_ws_production_aoas.csv") 
 wg_comp_aoas <- read_csv("aoas/eng_wg_production_aoas.csv") 
@@ -45,6 +47,34 @@ shinyServer(function(input, output) {
                    choices = choices,
                    selected = "production")
   })
+  ####### CHOOSE A SCALE FOR CUTOFF
+  output$cutoff <- renderUI({
+    req(input$source)
+    
+    if (input$source == "W2V"){
+      title <- "Normalized Cosine Similarity"
+      high_point <- .2
+      start_point <- .08
+      low_point <- 0
+      step_size <- .01
+    # } else if (input$source == "PB"){
+    #   title <- "Cosine Similarity"
+    #   high_point <- 1
+    #   start_point <- .6
+    #   low_point <- -1
+    #   step_size <- .1
+    } else if (input$source == "MFN"){
+      title <- "Number of Shared Features"
+      high_point <- 4
+      start_point <- 2
+      low_point <- 1
+      step_size <- 1
+    }
+  
+    sliderInput("cutoff", label=title,
+                  min = low_point, max = high_point, 
+                  value = start_point, step = step_size)
+  })
 
   ########## READ IN AOAS
   aoa_data <- reactive({
@@ -63,7 +93,13 @@ shinyServer(function(input, output) {
   
   ########## READ IN ASSOCIATIONS
   assoc_mat <- reactive({
-    if (input$assocs == "conceptual") {
+    if (input$source == "MFR") {
+      assocs <- all_assocs
+    } else if (input$source == "W2V") {
+        assocs <- w2v_assocs
+    } else if (input$source == "PB") {
+      assocs <- pb_assocs
+    } else if (input$assocs == "conceptual") {
       assocs <- c_assocs 
     } else if (input$assocs == "perceptual") {
       assocs <- p_assocs
@@ -87,7 +123,7 @@ shinyServer(function(input, output) {
                               stringsAsFactors = FALSE) %>%
       mutate(id = 1:n(), 
              identity = 1) %>%
-      left_join(aoa_data()) %>%
+      left_join(aoa_data()) %>% #left_join in wide ? format version of english item csv with category
       filter(!is.na(aoa), aoa <= input$age) %>%
       select_("label", "id", input$group) %>%
       rename_("group" = input$group)
@@ -109,15 +145,18 @@ shinyServer(function(input, output) {
       rename(out_node = id) %>%
       select(in_node, out_node, width)
   })
-  
+      
   ########## FILTER EDGES 
   assoc_edges <- reactive({
     req(input$weighted)
     
+    scaling = ifelse(input$source == "W2V", 15, 1)
+    
     #print(assoc_nodes())
     
     edges <- assoc_edge_data() %>%
-      filter(width >= input$cutoff) 
+      mutate(width = scaling*width) %>%
+      filter(width >= (scaling*input$cutoff)) 
     
     if (input$weighted == "TRUE") {
       edges
@@ -135,7 +174,7 @@ shinyServer(function(input, output) {
                rename(assoc_edges(), from = in_node, to = out_node), 
                width = "100%", height="100%") %>%
       visPhysics(stabilization = TRUE) %>%
-      visEdges(smooth = FALSE)
+      visEdges(smooth = FALSE, selfReferenceSize= FALSE)
     
   })
   
