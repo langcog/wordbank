1d46da3581c742f09f4dc6efa039bedebcc4d158
Danielle <danielle@psy-dk-md1.stanford.edu>
2016-10-11 16:50:46 -0700

    Mandarin IC raw data cleanup

--- incoming_data/Mandarin_Li/convert_li_IC.R
+++ incoming_data/Mandarin_Li/convert_li_IC.R
@@ -19,31 +19,31 @@ read_ping <- function(age, file) {
   ping_t[,c(a,(a+1),1:(a-1))]
 }
 
+item_names <- paste(1:nrow(instrument),instrument$item,sep="_")
 ping_all_comp <- bind_rows(mapply(read_ping, seq(12,16,1), ping_comp))
 ping_all_comp$V233 <- NULL
-colnames(ping_all_comp)[3:ncol(ping_all_comp)] <- paste0(instrument$item,1:nrow(instrument),"u", sep="_")
+colnames(ping_all_comp)[3:ncol(ping_all_comp)] <- paste0(item_names,"u")
 ping_all_comp[is.na(ping_all_comp)] <- ""
 ping_all_comp$study_id[ping_all_comp$study_id=="S372"] <- "S1372"
 
 ping_all_prod <- bind_rows(mapply(read_ping, seq(12,16,1), ping_prod))
-colnames(ping_all_prod)[3:ncol(ping_all_prod)] <- paste0(instrument$item,1:nrow(instrument),"p", sep="_")
+colnames(ping_all_prod)[3:ncol(ping_all_prod)] <- paste0(item_names,"p")
 ping_all_prod[is.na(ping_all_prod)] <- ""
 ping_all_prod$study_id[ping_all_prod$study_id=="1208.000000"] <- "S1208"
 
 a <- anti_join(ping_all_comp,ping_all_prod)
 ping_all <- left_join(ping_all_prod, ping_all_comp)
-sorted_items <- paste0(rep(colnames(ping_all_prod, each = 2)))
+sorted_items <- paste0(rep(item_names, each=2),c("u","p"))
 ping_sorted <- ping_all[,c("study_id","age", sorted_items)]
 
-data_file <- file("MandarinTC_Li_data.csv",
+data_file <- file("MandarinIC_Li_data.csv",
                   encoding = "utf8")
 write.csv(ping_sorted, row.names = FALSE,
           file = data_file)
 
-cols <- instrument$itemID
 fields <- data.frame(field = paste("item", 1:nrow(instrument), sep = "_"),
-                     column = cols,
+                     column = item_names,
                      group = "item",
                      type = "word")
 write.csv(fields, row.names = FALSE,
-          file = "MandarinTC_Li_fields.csv")
+          file = "MandarinIC_Li_fields.csv")
