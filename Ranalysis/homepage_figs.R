da8977c8fc586139665caa880a22fbd4271a187c
Danielle Kellier <danielle@DN0a22106d.SUNet>
2017-03-31 12:04:39 -0700

    About page tweaks and first draft graphs

--- Ranalysis/homepage_figs.R
+++ Ranalysis/homepage_figs.R
@@ -10,6 +10,8 @@ library(tidyverse)
 library(quantregGrowth)
 library(wordbankr)
 library(xkcd)
+library(stringr)
+library(directlabels)
 
 d <- get_administration_data(language = "English", form = "WG")
 
@@ -27,13 +29,55 @@ preds <- predictQR_fixed(mod, newdata = newdata) %>%
   gather(percentile, pred, starts_with("X")) %>%
   mutate(percentile = as.character(as.numeric(str_replace(percentile, "X", "")) * 100))
 
+
+
+
 ggplot(preds, aes(x = age, y = pred)) + 
   geom_line(aes(col = percentile)) + 
   ylab("VOCAB") + 
   xkcdaxis(xrange = c(7,19), yrange = c(0,400)) + 
   theme_xkcd() + 
-  theme(axis.line.x = element_line(colour="white"), 
-        panel.border = element_rect(size= 0, colour = "white"))
+  theme(axis.line.x = element_blank(), 
+        panel.border = element_blank())
 
 
 
+baby_words <- c("dog", "cat", "elephant", "rooster", "zebra")
+
+j <- get_item_data(language = "English", form = "WS") %>% filter(definition %in% baby_words)
+
+item_data <- get_instrument_data(instrument_language = "English", instrument_form = "WS", items = j$item_id, iteminfo = T, administrations = T) %>%
+  filter(!is.na(data_id)) %>%
+  mutate(numvalue = ifelse(value == "produces",1,0))
+
+group_data <- item_data %>%
+  group_by(definition, age) %>%
+  summarise(prop = mean(numvalue, na.rm=T))
+
+ggplot(group_data, aes(x = age, y = prop, colour = definition, fill = definition, label = definition)) +
+  geom_smooth(method = "loess",
+              se = FALSE) +
+  scale_x_continuous(name = "\nAge (months)",
+                     breaks = seq(min(group_data$age),max(group_data$age),4),
+                     limits = c(min(group_data$age), max(group_data$age) + 3)) +
+  scale_y_continuous(name = sprintf("%s\n", "Proportion of Children Producing"),
+                     limits = c(-0.01, 1),
+                     breaks = seq(0, 1, 0.25)) +
+  geom_dl(method = list(dl.trans(x = x + 0.3), "last.qp", cex = 1,
+                        fontfamily = "OpenSans")) +
+  ggthemes::theme_few(base_size = 14) +
+  theme(legend.position = "none",
+        panel.border = element_blank(),
+        axis.line = element_line(colour = "black"))
+
+ggplot(group_data, aes(x = age, y = prop)) +
+  geom_smooth(method = "loess",
+              se = FALSE, aes(colour = definition)) +
+  ylab("Proportion of Children Producing") +
+  geom_dl(method = list(dl.trans(x = x + 0.3), "last.qp", cex = 1,
+                        fontfamily = "xkcd"), aes(label = definition, colour = definition)) +
+  xkcdaxis(xrange = c(min(group_data$age),max(group_data$age) + 3), yrange = c(-0.01, 1)) + 
+  theme_xkcd() + 
+  theme(axis.line.x = element_blank(), 
+        panel.border = element_blank(),
+        legend.position = "none")
