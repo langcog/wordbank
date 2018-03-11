ba3c353b915c53283b5980c93d450835f96418f9
Danielle Kellier <dkellier@stanford.edu>
2018-03-11 16:29:22 -0700

    Add mom_ed and birth_order to Korean_Yim data

--- incoming_data/Korean_Yim/add_birthorder_and_momed.R
+++ incoming_data/Korean_Yim/add_birthorder_and_momed.R
@@ -2,20 +2,20 @@ library(tidyverse)
 library(readxl)
 
 wg_demos <- read_excel("incoming_data/Korean_Yim/K-CDI_data_totals_extra info.xlsx", sheet = 1) %>%
-  select(`X__1`, `subject ID`, `Test date`, `birth order`, `maternal education`)
-  
+  select(`subject ID`, `birth order`, `maternal education`)
 
 ws_demos <- read_excel("incoming_data/Korean_Yim/K-CDI_data_totals_extra info.xlsx", sheet = 2) %>%
-  select(`X__1`, `subject ID`, `Test date`, `Birth order`, `Maternal Education`)
+  select(`subject ID`, `Birth order`, `Maternal Education`)
 
-wg_data <- read_csv("raw_data/Korean_WG/KoreanWG_Yim_data.csv")
+wg_data_loc <- "raw_data/Korean_WG/KoreanWG_Yim_data.csv"
+wg_data <- read_csv(wg_data_loc) %>%
+  left_join(wg_demos, by = c("subj_num" = "subject ID")) %>%
+  select(s_no:age_months, `birth order`, `maternal education`, item_001:item_344)
 
-ws_data <- read_csv("raw_data/Korean_WS/KoreanWS_Yim_data.csv") %>%
-  mutate(subj_num = case_when(
-    subj_num == "201" ~ "TO3001",
-    subj_num == "202" ~ "TO3002",
-    subj_num == "203" ~ "TO3003",
-    subj_num == "204" ~ "TO3004",
-    subj_num == "205" ~ "TO3005",
-    TRUE ~ subj_num
-  ))
+ws_data_loc <- "raw_data/Korean_WS/KoreanWS_Yim_data.csv"
+ws_data <- read_csv(ws_data_loc)%>%
+  left_join(ws_demos, by = c("subj_num" = "subject ID")) %>%
+  select(s_no:age_months, `Birth order`, `Maternal Education`, item_001:item_679)
+
+write_csv(wg_data, wg_data_loc, na = "")
+write_csv(ws_data, ws_data_loc, na = "")
