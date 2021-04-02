675be229af77a4ebc449c86f7704769e9165e63b
HenryMehta <hjsmehta@gmail.com>
2021-04-02 11:36:00 +0100

    py38

--- common/management/commands/populate_source.py
+++ common/management/commands/populate_source.py
@@ -1,5 +1,8 @@
-import json
+import json, os
 from django.core.management.base import BaseCommand
+from django.conf import settings
+from django.templatetags.static import static
+
 from common.models import *
 
 
@@ -9,8 +12,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        sources = json.load(open('static/json/datasets.json'))
-
+        #sources = json.load(open('static/json/datasets.json'))
+        sources = json.load(open(os.path.realpath(settings.BASE_DIR + static('json/datasets.json')), encoding="utf8"))
         for source in sources:
 
             data_dict = {'contributor': source['contributor'],
@@ -19,11 +22,14 @@ class Command(BaseCommand):
                          'license': source['license']
             }
 
+            if source['name'] == 'CLEX':
+                print (source)
+
             data_set, created = Source.objects.update_or_create(
-                                     name=source['name'],
-                                     dataset=source['dataset'],
-                                     instrument_language=source['instrument_language'],
-                                     instrument_form=source['instrument_form'],
-                                     defaults=data_dict
-                                     )
+                name=source['name'],
+                dataset=source['dataset'],
+                instrument_language=source['instrument_language'],
+                instrument_form=source['instrument_form'],
+                defaults=data_dict
+                )
 
