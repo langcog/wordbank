6d1f6cce7e9e467d4c26844890729a63bda0b18d
HenryMehta <hjsmehta@gmail.com>
2021-04-02 09:04:24 +0100

    py38

--- .platform/hooks/postdeploy/01_django.sh
+++ .platform/hooks/postdeploy/01_django.sh
@@ -4,9 +4,9 @@ source "$PYTHONPATH/activate" && {
 # migrate
 if [[ $EB_IS_COMMAND_LEADER == "true" ]];
 then
-    python ./src/manage.py migrate --noinput;
-    python ./src/manage.py collectstatic --noinput;
-    python ./src/manage.py createsu;
+    python ./manage.py migrate --noinput;
+    python ./manage.py collectstatic --noinput;
+    python ./manage.py createsu;
 else
     echo "this instance is NOT the leader"
 fi
