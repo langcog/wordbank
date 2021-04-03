ca2e160d6013827e728ba1ec1c6d6d31b94700b2
HenryMehta <hjsmehta@gmail.com>
2021-04-03 13:44:13 +0100

    py38

--- wordbank/views.py
+++ wordbank/views.py
@@ -1,4 +1,8 @@
 from django.views.generic import TemplateView
+from django.http import HttpResponse
 
 class HealthView(TemplateView):
     template_name="health.html"
+
+    def get(self, request):
+        return HttpResponse(status=204)
