458a9ccba9f130e6ec5cc01786470cf0fb910dc5
HenryMehta <hjsmehta@gmail.com>
2021-04-19 20:50:26 +0100

    English Australian

--- .platform/hooks/postdeploy/01_django.sh
+++ .platform/hooks/postdeploy/01_django.sh
@@ -6,13 +6,6 @@ source "$PYTHONPATH/activate" && {
 python ./manage.py migrate --noinput;
 python ./manage.py createsu;
 python ./manage.py collectstatic --noinput;
-python ./manage.py delete_all_tables
-python ./manage.py populate_instrument
-python ./manage.py populate_category
-python ./manage.py populate_items 
-python ./manage.py populate_momed
-python ./manage.py populate_source
-python ./manage.py import_datasets -l 'American Sign Language'
-python ./manage.py import_datasets -l 'English (Australian)'
+python ./manage.py import_datasets -l 'British Sign Language'
 
 }
