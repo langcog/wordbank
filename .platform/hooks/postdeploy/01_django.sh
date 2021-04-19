2fb16402963db5a6bfa6add8e131b6dde7f266bc
HenryMehta <hjsmehta@gmail.com>
2021-04-19 20:01:37 +0100

    reload

--- .platform/hooks/postdeploy/01_django.sh
+++ .platform/hooks/postdeploy/01_django.sh
@@ -7,9 +7,5 @@ python ./manage.py migrate --noinput;
 python ./manage.py createsu;
 python ./manage.py collectstatic --noinput;
 python ./manage.py delete_all_tables
-python ./manage.py populate_instrument
-python ./manage.py populate_category
-python ./manage.py populate_items 
-python ./manage.py populate_momed
-python ./manage.py populate_source
+python ./manage.py import_datasets -l ASL
 }
