e08b0bc6d1b0884f404e71b30a67e195bb289fbe
Henry Mehta <hjsmehta@gmail.com>
2022-03-29 11:56:06 +0100

    Dutch Swingley

--- instruments/models.py
+++ instruments/models.py
@@ -61,3 +61,4 @@ from .schemas.Irish_WS import *
 from .schemas.Finnish_WS import *
 from .schemas.Finnish_WS_Short import *
 from .schemas.American_Sign_Language_CDI import *
+from .schemas.Dutch_Swingley import *
