6446f19f149961397aeea1314bc01e06c1e4c849
Henry Mehta <hjsmehta@gmail.com>
2022-04-18 18:25:11 +0100

    ASL CDI 2 to understands, produces rather than 1, 2

--- .platform/hooks/postdeploy/02_django.sh
+++ .platform/hooks/postdeploy/02_django.sh
@@ -3,7 +3,7 @@
 source "$PYTHONPATH/activate" && {
 # specify the management commands you want to run here
 
-#python ./manage.py migrate --noinput;
+python ./manage.py migrate --noinput;
 python ./manage.py createsu;
 python ./manage.py collectstatic --noinput;
 
