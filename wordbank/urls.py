c8ebbaca746d9da95a70f4eb0e80a6142d0ff9a1
Mika Braginsky <mika.br@gmail.com>
2022-08-25 10:55:10 -0400

    add url serving db_args.json for wordbankr

--- wordbank/urls.py
+++ wordbank/urls.py
@@ -2,7 +2,7 @@
 from django.conf.urls import url
 from django.urls.conf import path
 from django.contrib import admin
-
+from django.views.generic import TemplateView
 
 from common.views import *
 from instruments.views import *
@@ -13,17 +13,17 @@ from . import views
 # admin.autodiscover()
 
 urlpatterns = urlpatterns = [
-    # Examples:
     path('admin/', admin.site.urls),
     url(r'^$', Home.as_view(), name='home'),
     url(r'about', About.as_view(), name='about'),
     url(r'analyses', Analyses.as_view(), name='analyses'),
     url(r'contributors', Contributors.as_view(), name='contributors'),
-    # url(r'publications', Publications.as_view(), name='publications'),
-    # url(r'stats', Stats.as_view(), name='stats'),
     url(r'blog', Blog.as_view(), name='blog'),
     url(r'faq', Faq.as_view(), name='faq'),
-    path('healthcheck/', views.health_view, name="healthcheck" )
+    path('healthcheck/', views.health_view, name="healthcheck"),
+
+    # connection arguments that wordbankr uses by default
+    url(r'db_args', TemplateView.as_view(template_name = "db_args.json"), name='db_args')
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
