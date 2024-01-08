8ce1cdd901887b3c5b4327569bc35976f0ccc5fc
HenryMehta <hjsmehta@gmail.com>
2024-01-08 08:56:08 +0000

    Japanese WS and WG

--- .platform/hooks/postdeploy/02_django.sh
+++ .platform/hooks/postdeploy/02_django.sh
@@ -2,6 +2,8 @@
 
 source "$PYTHONPATH/activate" && {
 # specify the management commands you want to run here
+python ./manage.py createsu;
+python ./manage.py collectstatic --noinput;
 
 #python ./manage.py migrate --noinput;
 #python ./manage.py createsu;
