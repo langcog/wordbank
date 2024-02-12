c5c784fe7c2a12e9dbaca4d385496b15eb0b9a2a
HenryMehta <hjsmehta@gmail.com>
2024-02-12 10:44:36 +0000

    Django 5 requirement update

--- wordbank/urls.py
+++ wordbank/urls.py
@@ -15,13 +15,13 @@ urlpatterns = urlpatterns = [
     path("admin/", admin.site.urls),
     path("", Home.as_view(), name="home"),
     path("about/", About.as_view(), name="about"),
-    path(r"data/", Analyses.as_view(), name="data"),
-    path(r"contributors/", Contributors.as_view(), name="contributors"),
-    path(r"blog/", Blog.as_view(), name="blog"),
-    path(r"faq/", Faq.as_view(), name="faq"),
+    path("data/", Analyses.as_view(), name="data"),
+    path("contributors/", Contributors.as_view(), name="contributors"),
+    path("blog/", Blog.as_view(), name="blog"),
+    path("faq/", Faq.as_view(), name="faq"),
     path("healthcheck/", views.health_view, name="healthcheck"),
     # connection arguments that wordbankr uses by default
-    path(r"db_args/", TemplateView.as_view(template_name="db_args.json"), name="db_args")
+    path("db_args/", TemplateView.as_view(template_name="db_args.json"), name="db_args")
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # Uncomment the next line to enable the admin:
